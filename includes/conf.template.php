92c05ba24ef46f64cb297ba04b90efd148438438
eocdev <eoin.oconchuir@ul.ie>
2011-03-04 15:01:37 +0000

    Remove config file from version control, add template config instead

---
+++ includes/conf.template.php
@@ -0,0 +1,46 @@
+<?php
+/** COPY TO conf.php **/
+
+/** Database **/
+$db = array();
+$db['database'] = 'rosettaplatform';
+$db['server'] = 'localhost';
+$db['username'] = 'urosetta';
+$db['password'] = '';
+$db['show_errors'] = 'y';
+$db['show_sql'] = 'n';
+$db['log_file'] = '';
+$db['analyse_queries'] = 'n';
+
+/** Email **/
+$email = array();
+$email['port'] = 25;
+$email['host'] = '';
+$email['username'] = '';
+$email['password'] = '';
+
+/** Site **/
+$site = array();
+$site['name'] = '';
+$site['url'] = '';
+$site['title'] = '';
+$site['meta_desc'] = '';
+$site['meta_key'] = '';
+
+/** Smarty **/
+$smarty = array();
+$smarty['lib'] = '/home/eoin/sites/rosettaplatform/Rosetta-Foundation/templating/Smarty-3.0.7/libs/';
+$smarty['templates'] = '/home/eoin/sites/rosettaplatform/Rosetta-Foundation/templating/';
+/*
+	$smarty['lib']
+		The Smarty library files - downloaded from smarty.net
+		Server default: /usr/local/lib/php/Smarty/
+		Note: 
+			You have to 'escape' backslashes such as 'C:\\http_root'
+			Must end with a slash.
+	$smarty['templates']
+		The Smarty template files - designed for the site
+		Server default: /home/focloirg/smarty/
+		Note: You have to 'escape' backslashes such as 'C:\\http_root'
+		Must end with a slash.
+*/
