fe4ffc2726624003bde57c79c424de2dcee3cdae
Eoin Ó Conchúir <eoin.oconchuir@ul.ie>
2012-01-16 11:56:29 +0000

    Smarty View for Slim framework

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
 require 'libs/Slim/Slim/Slim.php';
-require('app/includes/smarty.php');
+require_once 'app/Views/SmartyView.php';
+
+// Can we get away from the app's old system?
+//require('app/includes/smarty.php');
 
 $app = new Slim(array(
-    'debug' => true
+    'debug' => true,
+    'view' => new SmartyView(),
     'mode' => 'development' // default is development. TODO get from config file, or set in environment...... $_ENV['SLIM_MODE'] = 'production';
 ));
 
@@ -32,12 +36,13 @@ $app->get('/', function () use ($app) {
 	/**
 	 * Home page functionality
 	 */
-	if ($tasks = $s->stream->getStream(10))
-	{
+	/*
+	if ($tasks = $s->stream->getStream(10) {
 		$s->assign('tasks', $tasks);
 	}
-	$s->display('index.tpl');
-    $app->render('foo.php');
+	*/
+	//$s->display('index.tpl');
+    $app->render('index.tpl');
 });
 
 $app->run();
