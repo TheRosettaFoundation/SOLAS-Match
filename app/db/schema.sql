aeda3a22f69b0ce6750679c30da801f17110000f
Eoin Ó Conchúir <eoin.oconchuir@ul.ie>
2012-04-04 15:49:23 +0100

    solasmatch schema version

---
+++ app/db/schema.sql
@@ -0,0 +1,207 @@
+-- MySQL dump 10.13  Distrib 5.1.61, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: solasmatch
+-- ------------------------------------------------------
+-- Server version	5.1.61-0ubuntu0.11.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `language`
+--
+
+DROP TABLE IF EXISTS `language`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `language` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `code` varchar(5) NOT NULL COMMENT '"en", for example',
+  `en_name` varchar(255) NOT NULL COMMENT '"English", for example',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `old_task_file`
+--
+
+DROP TABLE IF EXISTS `old_task_file`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `old_task_file` (
+  `task_id` bigint(20) unsigned NOT NULL,
+  `file_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `path` text NOT NULL,
+  `filename` text NOT NULL,
+  `content_type` varchar(255) NOT NULL COMMENT 'Mime type',
+  `user_id` int(11) DEFAULT NULL COMMENT 'Can be null while users table is empty! Remove this option once logins working',
+  `upload_time` datetime NOT NULL,
+  PRIMARY KEY (`task_id`,`file_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `organisation`
+--
+
+DROP TABLE IF EXISTS `organisation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `organisation` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `organisation_member`
+--
+
+DROP TABLE IF EXISTS `organisation_member`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `organisation_member` (
+  `user_id` int(10) unsigned NOT NULL,
+  `organisation_id` int(10) unsigned NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  KEY `user_id` (`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `tag`
+--
+
+DROP TABLE IF EXISTS `tag`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tag` (
+  `tag_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `label` varchar(50) CHARACTER SET latin1 NOT NULL,
+  PRIMARY KEY (`tag_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `task`
+--
+
+DROP TABLE IF EXISTS `task`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `task` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `organisation_id` int(10) unsigned NOT NULL,
+  `title` text NOT NULL,
+  `word_count` int(10) unsigned DEFAULT NULL,
+  `source_id` int(10) unsigned DEFAULT NULL COMMENT 'foreign key from the `language` table',
+  `target_id` int(10) unsigned DEFAULT NULL COMMENT 'foreign key from the `language` table',
+  `created_time` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `source` (`source_id`),
+  KEY `target` (`target_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=113 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `task_file_version`
+--
+
+DROP TABLE IF EXISTS `task_file_version`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `task_file_version` (
+  `task_id` bigint(20) NOT NULL,
+  `version_id` int(11) NOT NULL COMMENT 'Gets incremented within the code',
+  `filename` text NOT NULL,
+  `content_type` varchar(255) NOT NULL,
+  `user_id` int(11) DEFAULT NULL COMMENT 'Null while we don''t have logging in',
+  `upload_time` datetime NOT NULL,
+  KEY `task_id` (`task_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `task_file_version_download`
+--
+
+DROP TABLE IF EXISTS `task_file_version_download`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `task_file_version_download` (
+  `task_id` bigint(20) unsigned NOT NULL,
+  `file_id` int(10) unsigned NOT NULL,
+  `version_id` int(10) unsigned NOT NULL,
+  `user_id` int(10) unsigned DEFAULT NULL,
+  `time_downloaded` datetime NOT NULL,
+  KEY `task_id` (`task_id`,`file_id`,`version_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `task_tag`
+--
+
+DROP TABLE IF EXISTS `task_tag`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `task_tag` (
+  `task_id` bigint(20) unsigned NOT NULL,
+  `tag_id` int(10) unsigned NOT NULL,
+  `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  UNIQUE KEY `task_tag` (`task_id`,`tag_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `translator`
+--
+
+DROP TABLE IF EXISTS `translator`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `translator` (
+  `user_id` int(11) NOT NULL,
+  `role_added` datetime NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(256) NOT NULL,
+  `password` char(128) NOT NULL,
+  `nonce` int(11) unsigned NOT NULL,
+  `created_time` datetime NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=MyISAM AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2012-04-04 15:49:23
