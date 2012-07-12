da4b90336e6c648d8c089661dd2cbfff82742b87
spaceindaver <spaceindaver0@gmail.com>
2012-07-12 15:41:38 +0100

    Aesthetic updates to the task page
    Update the task page to better accomodate the document viewer.
    Closes issue #45

--- index.php
+++ index.php
@@ -320,7 +320,8 @@ $app->get('/task/id/:task_id/', function ($task_id) use ($app) {
     $appPos = strrpos($file_path, "app", $searchStart);
     $file_path = "http://".$_SERVER["HTTP_HOST"].$app->urlFor('home').substr($file_path, $appPos);
     $app->view()->appendData(array(
-        'file_preview_path' => $file_path
+        'file_preview_path' => $file_path,
+        'file_name' => $task_file_info['filename']
     ));
 
     if ($task_dao->taskIsClaimed($task)) {
