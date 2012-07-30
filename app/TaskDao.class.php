TheRosettaFoundation/SOLAS-Match

commit d9724a91284088dd3302755a679d3f4489ab18b6

Updated the Task stream on task pages
This commit updates the way tasks are gotten from the DB for task
pages. It changes the logic so it defaults to returning all tasks
unless specified otherwise. I have also updated the home page to
limit the number of tasks that will show up. closes #65

app/TaskDao.class.php
@@ -346,8 +346,10 @@ New requirement:
                         SELECT task_id
                         FROM task_claim
                     )
-                    ORDER BY uts.score DESC
-                    LIMIT '.$db->cleanse($nb_items);
+                    ORDER BY uts.score DESC';
+        if($nb_items != 0) {
+            $query .= ' LIMIT '.$db->cleanse($nb_items);
+        }
 
         $ret = false;
         if ($result = $db->Select($query)) {

index.php
@@ -149,7 +149,7 @@ $app->get('/', function () use ($app) {
             $app->view()->setData('tasks', $tasks);
         }
     } else {
-        if($tasks = TaskStream::getUserStream($current_user->getUserId())) {
+        if($tasks = TaskStream::getUserStream($current_user->getUserId(), 10)) {
             $app->view()->setData('tasks', $tasks);
         }
 
@@ -808,7 +808,7 @@ $app->get('/tasks/active/p/:page_no', function ($page_no) use ($app) {
     $user_dao = new UserDao();
 
     $user = $user_dao->getCurrentUser();
-    $activeTasks = $task_dao->getUserTasks($user);
+    $activeTasks = TaskStream::getUserStream($user->getUserId());
 
     $tasks_per_page = 10;
     $total_pages = ceil(count($activeTasks) / $tasks_per_page);

app/TaskStream.class.php
@@ -20,7 +20,7 @@ class TaskStream
     /*
         Same as above except take the user's preferences into account
     */
-    public static function getUserStream($user_id, $nb_items = 10) {
+    public static function getUserStream($user_id, $nb_items = 0) {
         $task_dao = new TaskDao();
         return $task_dao->getUserTopTasks($user_id, $nb_items);
     }

commit a766cd807d4130cffb2e2ac0369ba93c58dc566d

Update task logic and remove broken links.
Allow a limit to be put on the number of active/archived tasks that
get displayed on the user profile page (fixes bug introduced by
d9724a91284088dd3302755a679d3f4489ab18b6). Removes links to current
user's active/archived tasks pages when viewing another user's
profile. Closes #64

app/TaskDao.class.php
@@ -589,25 +589,29 @@ New requirement:
 		}
 	}
 
-    public function getUserTasks($user)
+    public function getUserTasks($user, $limit = 0)
     {
         $db = new MySQLWrapper();
         $db->init();
         $query = 'SELECT * 
                     FROM task JOIN task_claim ON task_claim.task_id = task.id
-                    WHERE user_id = '.$db->cleanse($user->getUserId()).'
-                    LIMIT 0, 10';
+                    WHERE user_id = '.$db->cleanse($user->getUserId());
+        if($limit != 0) {
+            $query .= ' LIMIT 0, 10';
+        }
         return $this->_parse_result_for_user_task($db->Select($query));
     }
 
-    public function getUserArchivedTasks($user)
+    public function getUserArchivedTasks($user, $limit = 0)
     {
         $db = new MySQLWrapper();
         $db->init();
         $query = 'SELECT *
                     FROM archived_task as a JOIN task_claim as c ON a.task_id = c.task_id
-                    WHERE user_id = '.$db->cleanse($user->getUserID()).'
-                    LIMIT 0, 10';
+                    WHERE user_id = '.$db->cleanse($user->getUserID());
+        if($limit != 0) {
+            $query .= ' LIMIT 0, 10';
+        }
         return $this->_parse_result_for_user_task($db->Select($query));
     }
 

app/templating/templates/user-public-profile.tpl
@@ -81,7 +81,10 @@
 {if isset($activeJobs)}
     {if count($activeJobs) > 0}
         <div class='page-header'><h1>Active Jobs <small>A list of jobs you are currently working on</small>
-        <a href='{urlFor name="active-tasks" options="page_no.1"}' class='pull-right btn btn-primary'>Active Tasks</a></h1></div>
+        {if isset($private_access)}
+            <a href='{urlFor name="active-tasks" options="page_no.1"}' class='pull-right btn btn-primary'>Active Tasks</a>
+        {/if}
+        </h1></div>
 
         {foreach $activeJobs as $job}
                 {include file="task.summary-link.tpl" task=$job}
@@ -92,7 +95,10 @@
 {if isset($archivedJobs)}
     {if count($archivedJobs) > 0}
         <div class='page-header'><h1>Archived Jobs <small>A list of jobs you have worked on in the past</small>
-        <a href='{urlFor name="archived-tasks" options="page_no.1"}' class='pull-right btn btn-primary'>Archived Tasks</a></h1></div>
+        {if isset($private_access)}
+            <a href='{urlFor name="archived-tasks" options="page_no.1"}' class='pull-right btn btn-primary'>Archived Tasks</a>
+        {/if}
+        </h1></div>
 
         {foreach $archivedJobs as $job}
             {include file="task.profile-display.tpl" task=$job}

index.php
@@ -728,9 +728,9 @@ $app->get('/profile/:user_id', function ($user_id) use ($app) {
     $user = $user_dao->find(array('user_id' => $user_id));
     
     $task_dao = new TaskDao();
-    $activeJobs = $task_dao->getUserTasks($user);
+    $activeJobs = $task_dao->getUserTasks($user, 10);
 
-    $archivedJobs = $task_dao->getUserArchivedTasks($user);
+    $archivedJobs = $task_dao->getUserArchivedTasks($user, 10);
 
     $user_tags = $user_dao->getUserTags($user->getUserId());
 
@@ -809,7 +809,7 @@ $app->get('/tasks/active/p/:page_no', function ($page_no) use ($app) {
     $user_dao = new UserDao();
 
     $user = $user_dao->getCurrentUser();
-    $activeTasks = TaskStream::getUserStream($user->getUserId());
+    $activeTasks = $task_dao->getUserTasks($user->getUserId());
 
     $tasks_per_page = 10;
     $total_pages = ceil(count($activeTasks) / $tasks_per_page);