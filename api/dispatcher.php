84a3c4b79c33d84201c0251abf1b01d659b700a4
Sean Mooney <sean.mooney@ul.ie>
2012-09-11 12:51:03 +0100

    added api helper class

--- api/dispatcher.php
+++ api/dispatcher.php
@@ -3,7 +3,8 @@ require '../vendor/autoload.php';
 require '../app/Settings.class.php';
 require_once 'FormatEnum.php';
 require_once 'HttpMethodEnum.php';
-require_once 'XML/Serializer.php';
+require_once '../app/APIHelper.php';
+
 
 
 class Dispatcher {
@@ -22,7 +23,8 @@ class Dispatcher {
        $path = explode("/", $path);
        $path =$path[1];
        $initFunc = "Dispatcher::init_".$path;
-        call_user_func($initFunc); 
+       call_user_func($initFunc); 
+       Dispatcher::getDispatcher()->run();
         
     }
     public  static function init_v0(){
@@ -30,24 +32,23 @@ class Dispatcher {
         require_once 'v0/Tasks.php';
         require_once 'v0/Tags.php';
         require_once 'v0/Badges.php';
-        require_once 'v0/Orgs.php';
-        Dispatcher::getDispatcher()->run();
+        require_once 'v0/Orgs.php'; 
     }
     
     public static function sendResponce($headers,$body,$code=200,$format=".json"){
         $response = Dispatcher::getDispatcher()->response();
         
-        $formatCode=  Dispatcher::getFormat($format); 
+        $formatCode=  APIHelper::getFormat($format); 
         switch ($formatCode){
             case FormatEnum::JSON: {
                 $response['Content-Type'] = 'application/json';
-                $body=Dispatcher::serialiser($body,$format);
+                $body=APIHelper::serialiser($body,$format);
                 break;
             }
             case FormatEnum::XML: {
                try{
                   $response['Content-Type'] = 'application/xml';
-                   $body=Dispatcher::serialiser($body,$format);
+                   $body=APIHelper::serialiser($body,$format);
                } catch (Exception $e)  {  echo $e;}  
                 break;
             }
@@ -55,7 +56,7 @@ class Dispatcher {
             case FormatEnum::HTML: {
                try{
                    $response['Content-Type'] = 'text/html';
-                   $body=Dispatcher::serialiser($body,$format);
+                   $body=APIHelper::serialiser($body,$format);
                } catch (Exception $e)  {  echo $e;}  
                 break;
             }
@@ -63,7 +64,7 @@ class Dispatcher {
             case FormatEnum::PHP:{
                try{
                   $response['Content-Type'] = 'text/plain';
-                  $body=Dispatcher::serialiser($body,$format);
+                  $body=APIHelper::serialiser($body,$format);
                } catch (Exception $e)  {  echo $e;}  
                 break;
             }
@@ -78,94 +79,7 @@ class Dispatcher {
         $response->status($code);
     }
     
-    public static function serialiser($body,$format=".json"){
-        $response = Dispatcher::getDispatcher()->response();
-        
-        $format=  Dispatcher::getFormat($format); 
-        switch ($format){
-            case FormatEnum::JSON: {
-                return json_encode($body);
-            }
-            case FormatEnum::XML: {
-               return wddx_serialize_value($body);
-            }
-            
-            case FormatEnum::HTML: {
-               return htmlspecialchars(wddx_serialize_value($body));
-            }
-            
-            case FormatEnum::PHP:{
-               return serialize($body);
-            }
-        }
-    }
-    public static function deserialiser($data,$format=".json"){
-        $format=  Dispatcher::getFormat($format); 
-        switch ($format){
-            case FormatEnum::JSON: {
-                try{
-                 return json_encode($body);
-                }catch (Exception $e){
-                    Dispatcher::sendResponce(null, "request format error. please resend in json or append .xml,.php,.html,.proto or .json as appropriate",400,".json");
-                }
-                break;
-            }
-            case FormatEnum::XML: {
-                try{
-                 return wddx_deserialize($body);
-                }catch (Exception $e){
-                    Dispatcher::sendResponce(null, "request format error. please resend in json or append .xml,.php,.html,.proto or .json as appropriate",400,".json");
-                }
-                break;
-            }
-            
-            case FormatEnum::HTML: {
-                try{
-                 return  wddx_deserialize(htmlspecialchars_decode($body));
-                }catch (Exception $e){
-                    Dispatcher::sendResponce(null, "request format error. please resend in json or append .xml,.php,.html,.proto or .json as appropriate",400,".json");
-                }
-                break;
-            }
-            
-            
-            
-            case FormatEnum::PHP:{
-              try{
-                 return unserialize($body);
-                }catch (Exception $e){
-                    Dispatcher::sendResponce(null, "request format error. please resend in json or append .xml,.php,.html,.proto or .json as appropriate",400,".json");
-                }
-                break;
-            }
-        }
-        
-    }
     
-    public static function call($url,$data,$httpMethod,$queryArgs=array(), $format=".php"){
-        $url.=$format."?";
-        foreach ($queryArgs as $key=>$val) $url.=$key."=".$val."&";
-         switch($httpMethod){
-            case HttpMethodEnum::DELETE:{
-                $request = new HttpRequest($url,  HttpRequest::METH_DELETE);
-                return Dispatcher::deserialiser($request->send()->getBody(),$format);
-            }
-            case HttpMethodEnum::GET:{
-                $request = new HttpRequest($url,  HttpRequest::METH_GET);
-                return Dispatcher::deserialiser($request->send()->getBody(),$format);
-            }
-            case HttpMethodEnum::POST:{
-                $request = new HttpRequest($url,  HttpRequest::METH_POST);
-                $request->setRawPostData(Dispatcher::serialiser($data,$format));
-                return Dispatcher::deserialiser($request->send()->getBody(),$format);
-            }
-            case HttpMethodEnum::PUT:{
-                $request = new HttpRequest($url,  HttpRequest::METH_PUT);
-                $request->setPutData(Dispatcher::serialiser($data,$format));
-                return Dispatcher::deserialiser($request->send()->getBody(),$format);
-            }
-        } 
-    }
     
     public static function register($httpMethod,$url,$function){
         switch($httpMethod){
@@ -209,16 +123,7 @@ class Dispatcher {
             }
         }
     }
-    public static function getFormat($format){
-       if($format==".json") $format=  FormatEnum::JSON;
-       elseif(strcasecmp($format,'.xml')==0) $format=  FormatEnum::XML;
-       elseif(strcasecmp($format,'.php')==0) $format=  FormatEnum::PHP;
-       elseif(strcasecmp($format,'.html')==0) $format=  FormatEnum::HTML;
-       elseif(strcasecmp($format,'.proto')==0) $format=  FormatEnum::JSON;//change when implmented.
-       else $format=  FormatEnum::JSON;
-       return $format;
-    }
-    
+   
 
 }
 Dispatcher::init();
---
+++ app/APIHelper.php
@@ -0,0 +1,119 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of APIHelper
+ *
+ * @author sean
+ */
+
+require_once 'FormatEnum.php';
+require_once 'HttpMethodEnum.php';
+require_once '../api/dispatcher.php';
+class APIHelper {
+    
+    public static function serialiser($body,$format=".json"){
+        $format=  APIHelper::getFormat($format); 
+        switch ($format){
+            case FormatEnum::JSON: {
+                return json_encode($body);
+            }
+            case FormatEnum::XML: {
+               return wddx_serialize_value($body);
+            }
+            
+            case FormatEnum::HTML: {
+               return htmlspecialchars(wddx_serialize_value($body));
+            }
+            
+            case FormatEnum::PHP:{
+               return serialize($body);
+            }
+        }
+    }
+    public static function deserialiser($data,$format=".json"){
+        $format=  APIHelper::getFormat($format); 
+        switch ($format){
+            case FormatEnum::JSON: {
+                try{
+                 return json_encode($body);
+                }catch (Exception $e){
+                    // change to exception and send responce from api
+                    Dispatcher::sendResponce(null, "request format error. please resend in json or append .xml,.php,.html,.proto or .json as appropriate",400,".json");
+                }
+                break;
+            }
+            case FormatEnum::XML: {
+                try{
+                 return wddx_deserialize($body);
+                }catch (Exception $e){
+                    Dispatcher::sendResponce(null, "request format error. please resend in json or append .xml,.php,.html,.proto or .json as appropriate",400,".json");
+                }
+                break;
+            }
+            
+            case FormatEnum::HTML: {
+                try{
+                 return  wddx_deserialize(htmlspecialchars_decode($body));
+                }catch (Exception $e){
+                    Dispatcher::sendResponce(null, "request format error. please resend in json or append .xml,.php,.html,.proto or .json as appropriate",400,".json");
+                }
+                break;
+            }
+            
+            
+            
+            case FormatEnum::PHP:{
+              try{
+                 return unserialize($body);
+                }catch (Exception $e){
+                    Dispatcher::sendResponce(null, "request format error. please resend in json or append .xml,.php,.html,.proto or .json as appropriate",400,".json");
+                }
+                break;
+            }
+        }
+        
+    }
+    
+    public static function call($url,$data,$httpMethod,$queryArgs=array(), $format=".php"){
+        $url.=$format."?";
+        foreach ($queryArgs as $key=>$val) $url.=$key."=".$val."&";
+         switch($httpMethod){
+            case HttpMethodEnum::DELETE:{
+                $request = new HttpRequest($url,  HttpRequest::METH_DELETE);
+                return APIHelper::deserialiser($request->send()->getBody(),$format);
+            }
+            case HttpMethodEnum::GET:{
+                $request = new HttpRequest($url,  HttpRequest::METH_GET);
+                return APIHelper::deserialiser($request->send()->getBody(),$format);
+            }
+            case HttpMethodEnum::POST:{
+                $request = new HttpRequest($url,  HttpRequest::METH_POST);
+                $request->setRawPostData(Dispatcher::serialiser($data,$format));
+                return APIHelper::deserialiser($request->send()->getBody(),$format);
+            }
+            case HttpMethodEnum::PUT:{
+                $request = new HttpRequest($url,  HttpRequest::METH_PUT);
+                $request->setPutData(Dispatcher::serialiser($data,$format));
+                return APIHelper::deserialiser($request->send()->getBody(),$format);
+            }
+        } 
+    }
+    
+     public static function getFormat($format){
+       if($format==".json") $format=  FormatEnum::JSON;
+       elseif(strcasecmp($format,'.xml')==0) $format=  FormatEnum::XML;
+       elseif(strcasecmp($format,'.php')==0) $format=  FormatEnum::PHP;
+       elseif(strcasecmp($format,'.html')==0) $format=  FormatEnum::HTML;
+       elseif(strcasecmp($format,'.proto')==0) $format=  FormatEnum::JSON;//change when implmented.
+       else $format=  FormatEnum::JSON;
+       return $format;
+    }
+    
+}
+
+?>
