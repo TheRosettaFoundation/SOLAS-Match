cfe362eebcb0d8ae99b45d148f9419bbeaa55adf
spaceindaver <spaceindaver0@gmail.com>
2012-10-24 15:50:04 +0100

    Fix Google Docs preview
    Change how the file is passed to google docs so that a task can
    have whitespace in its filename. Closes #288

--- ui/TaskRouteHandler.class.php
+++ ui/TaskRouteHandler.class.php
@@ -427,10 +427,10 @@ class TaskRouteHandler
         }
         
         $task_file_info = TaskFile::getTaskFileInfo($task, 0);
-        $file_path = Upload::absoluteFilePathForUpload($task, 0, $task_file_info['filename']);
-        $searchStart = strlen($file_path) - strrpos($file_path, $task_file_info['filename']);
-        $appPos = strrpos($file_path, "app", $searchStart);
-        $file_path = "http://".$_SERVER["HTTP_HOST"].$app->urlFor('home').substr($file_path, $appPos);
+        $file_path = dirname(Upload::absoluteFilePathForUpload($task, 0, $task_file_info['filename']));
+        $appPos = strrpos($file_path, "app");
+        $file_path = "http://".$_SERVER["HTTP_HOST"].$app->urlFor('home').
+                substr($file_path, $appPos).'/'.$task_file_info['filename'];
         $app->view()->appendData(array(
             'file_preview_path' => $file_path,
             'file_name' => $task_file_info['filename']
