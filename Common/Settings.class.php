59400af09b1ed038f2476d54874d6c3d923a553b
Manuel Honegger <manuel_honegger@outlook.com>
2012-12-11 12:50:27 +0000

    Updated Settings class to PSR

--- Common/Settings.class.php
+++ Common/Settings.class.php
@@ -1,35 +1,37 @@
 <?php
-// http://www.phpit.net/article/create-settings-class-php/
-// Loads the file /conf/conf.php.
 
-Class Settings {
-    var $_settings = array();
+class Settings {
 
-    function __construct() {
+    private $_settings = array();
+
+    public function __construct()
+    {
         $file = dirname(__FILE__).'/includes/conf.ini';
         $this->load($file);
     }
 
-    public function get($var) {
-        $result = $this->_retrieveValue($var);
+    public function get($var)
+    {
+        $result = $this->retrieveValue($var);
         if (is_null($result)) {
             throw new BadMethodCallException('Could not load the requested setting ' . $var);
         }
         return $result;
     }
 
-    private function _retrieveValue($var) {
+    private function retrieveValue($var)
+    {
         $var = explode('.', $var);
         if (isset($this->_settings[$var[1]])) {
             return $this->_settings[$var[1]];
-        }
-        else {
+        } else {
             return null;
         }
     }
 
-	function load($file) {
-        if(file_exists($file)) {
+    public function load($file)
+    {
+        if (file_exists($file)) {
             $this->_settings = parse_ini_file($file);
             //This updates the upload path to be absolute
             $this->_settings['upload_path'] = __DIR__."/../".$this->_settings['upload_path'];
