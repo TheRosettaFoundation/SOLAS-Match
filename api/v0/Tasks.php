27fdaffe67a4fdce815254daa6fea4f7897fdeca
Manuel Honegger <manuel_honegger@outlook.com>
2013-03-09 18:55:39 +0000

    Fixed getProjectFileInfo call in tasks api

--- api/v0/Tasks.php
+++ api/v0/Tasks.php
@@ -226,7 +226,7 @@ class Tasks {
             }
             
             $projectDao = new ProjectDao();
-            $projectFile = $projectDao->getProjectFileInfo($task->getProjectId(), $userID, null, null, null);
+            $projectFile = $projectDao->getProjectFileInfo($task->getProjectId(), null, null, null, null);
             $filename = $projectFile->getFilename();
             
             $convert = Dispatcher::clenseArgs('convertFromXliff', HttpMethodEnum::GET, false);
