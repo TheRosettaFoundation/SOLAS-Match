15d76a0fd120c829fd5dd3ad995e2541ceb445eb
SeanMooney <Sean.Mooney@ul.ie>
2013-03-21 19:25:03 +0000

    update dbTeardown funchtion

--- test/UnitTestHelper.php
+++ test/UnitTestHelper.php
@@ -6,14 +6,14 @@ require_once __DIR__.'/../Common/lib/PDOWrapper.class.php';
 class UnitTestHelper
 {
     private function __constuct() {}
-    
+    private static $initalised = false;
     public static function teardownDb()
     {
-        $dsn = "mysql:host=".Settings::get('unit_test.server').";dbname=".Settings::get('unit_test.database').
+        $dsn = "mysql:host=".Settings::get('unit_test.server').
                 ";port=".Settings::get('unit_test.port');
         $dsn1 = "mysql:host=".Settings::get('database.server').";dbname=".Settings::get('database.database').
                 ";port=".Settings::get('database.server_port');
-        assert($dsn1 != $dsn);
+        assert($dsn1 != $dsn &&Settings::get('database.database')!=Settings::get('unit_test.database'));
         
         PDOWrapper::$unitTesting = true;
         $conn = new PDO($dsn,
@@ -21,15 +21,48 @@ class UnitTestHelper
                         array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));         
         unset($dsn);
         unset($dsn1);
-        $tables = $conn->query("SELECT t.TABLE_NAME FROM information_schema.`TABLES` t WHERE t.TABLE_SCHEMA='Unit-Test'
-                                AND t.TABLE_NAME NOT IN('Languages','Countries', 'TaskTypes', 'TaskStatus')");
-        
-        foreach($tables as $table) $conn->query("DELETE FROM $table[0]");
-        
-        $conn->query("REPLACE INTO `Badges` (`id`, `owner_id`, `title`, `description`) VALUES
-                    (3, NULL, 'Profile-Filler', 'Filled in required info for user profile.'),
-                    (4, NULL, 'Registered', 'Successfully set up an account'),
-                    (5, NULL, 'Native-Language', 'Filled in your native language on your user profile.');");
+        if(!self::$initalised){
+           
+           $result=$conn->exec("drop database `".Settings::get('unit_test.database')."`");
+           $result=$conn->exec("CREATE DATABASE `".Settings::get('unit_test.database')."` /*!40100 CHARACTER SET utf8 COLLATE 'utf8_unicode_ci' */");
+           $result=$conn->exec("use `".Settings::get('unit_test.database')."`");
+           
+           $schema = file_get_contents(__DIR__.'/../db/schema-min.sql');
+           $schema = str_replace("DELIMITER //", "", $schema);
+           $schema = str_replace("DELIMITER ;", "", $schema);
+           $schema = str_replace("END//", "END;", $schema);
+           
+           $result=$conn->exec($schema);
+           $schema = file_get_contents(__DIR__.'/../db/languages.sql');
+           $result=$conn->exec($schema);
+           $schema = file_get_contents(__DIR__.'/../db/country_codes.sql');
+           $result=$conn->exec($schema);
+//           $result=$conn->exec("DROP PROCEDURE IF EXISTS `userUnTrackProject`;");
+//           $result=$conn->exec("DELIMITER //
+//CREATE  PROCEDURE `userUnTrackProject`(IN `pID` INT, IN `uID` INT)
+//BEGIN
+//	if exists (select 1 from UserTrackedProjects utp where utp.user_id=uID and utp.Project_id=pID) then
+//		delete from UserTrackedProjects  where user_id=uID and Project_id=pID;
+//		select 1 as result;
+//	else
+//		select 0 as result;
+//	end if;
+//END//
+//DELIMITER ;");
+           self::$initalised=true;
+        }else{
+            $result=$conn->exec("use `".Settings::get('unit_test.database')."`");
+            $tables = $conn->query("SELECT t.TABLE_NAME FROM information_schema.`TABLES` t WHERE t.TABLE_SCHEMA='Unit-Test'
+                                    AND t.TABLE_NAME NOT IN('Languages','Countries', 'TaskTypes', 'TaskStatus')");
+
+            foreach($tables as $table) $conn->exec("DELETE FROM $table[0]");
+
+            $conn->exec("REPLACE INTO `Badges` (`id`, `owner_id`, `title`, `description`) VALUES
+                        (3, NULL, 'Profile-Filler', 'Filled in required info for user profile.'),
+                        (4, NULL, 'Registered', 'Successfully set up an account'),
+                        (5, NULL, 'Native-Language', 'Filled in your native language on your user profile.');");
+            $conn->exec("ALTER TABLE `Badges` AUTO_INCREMENT=100;");
+        }
     }
     
     
