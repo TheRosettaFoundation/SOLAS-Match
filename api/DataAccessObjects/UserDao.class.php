3e37bade3db6180da395b11a8b2d7e90b844efa4
Manuel Honegger <manuel_honegger@outlook.com>
2013-05-02 20:30:02 +0100

    Fixed register to return user object

--- api/DataAccessObjects/UserDao.class.php
+++ api/DataAccessObjects/UserDao.class.php
@@ -122,12 +122,12 @@ class UserDao
         if (!is_object($user) && $clear_password != "") {
             $user = self::create($email, $clear_password);
             BadgeDao::assignBadge($user->getId(), BadgeTypes::REGISTERED);
-            $ret = self::registerUser($user->getId());
+            self::registerUser($user->getId());
         }
 
         Notify::sendEmailVerification($user->getId());
 
-        return $ret;
+        return $user;
     }
 
     private static function registerUser($userId)
@@ -138,7 +138,7 @@ class UserDao
         $args .= PDOWrapper::cleanseNullOrWrapStr($uid);
         $result = PDOWrapper::call("registerUser", $args);
         if ($result) {
-            $ret = $result;
+            $ret = $result[0]['result'];
         }
         return $ret;
     }
