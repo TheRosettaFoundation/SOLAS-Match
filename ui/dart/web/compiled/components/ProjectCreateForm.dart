c8eb801fb661b262fbd46ea536830b9192eee38c
David O Carroll <spaceindaver0@gmail.com>
2013-08-27 16:09:07 +0100

    Don't show segmentation message if segmentation is selected
    Related to word counts. Closes #489

--- ui/dart/web/compiled/components/ProjectCreateForm.dart
+++ ui/dart/web/compiled/components/ProjectCreateForm.dart
@@ -1270,7 +1270,17 @@ class ProjectCreateForm extends WebComponent with Observable
         return 0;
       });
       if (project.wordCount > 5000) {
-        if (!window.confirm(Localisation.getTranslation("project_create_22"))) {
+        int i = 0;
+        bool segmentationMissing = false;
+        CheckboxInputElement segmentationCheckbox; 
+        while (i < targetCount && !segmentationMissing) {
+          segmentationCheckbox = query("#segmentation_$i");
+          if (!segmentationCheckbox.checked) {
+            segmentationMissing = true;
+          }
+          i++;
+        }
+        if (segmentationMissing && !window.confirm(Localisation.getTranslation("project_create_22"))) {
           ret = false;
         }
       }
