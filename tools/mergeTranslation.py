d66070587d5cadc060a0510ba476fb6a69146496
David O Carroll <spaceindaver0@gmail.com>
2014-02-18 13:15:14 +0000

    Added tool for merging site translations
    Adds a tool that takes 3 files, the original english strings file,
    the current version of a specific language strings file and a
    file containing all the new strings for that language. It then
    parses all the files and merges the third file with the second.
    Needs an update to merge new strings. Part of #740

---
+++ tools/mergeTranslation.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import xml.etree.ElementTree as ET
+import sys, getopt
+
+def printUsage():
+    print 'usage: mergeTranslation.py --englishFile <englishFile> --originalFile <originalFile> --translatedFile <translatedFile> [-o <output>]'
+
+def main(argv):
+    englishFile = ''
+    originalFile = ''
+    translatedFile = ''
+    outputFile = 'merged_strings.xml'
+    try:
+        opts, args = getopt.getopt(argv, "h", ["englishFile=", "originalFile=", "translatedFile="])
+    except getopt.GetoptError:
+        printUsage()
+        sys.exit(2)
+    for opt, arg, in opts:
+        if opt == '-h':
+            printUsage()
+            sys.exit()
+        elif opt == '--englishFile':
+            englishFile = arg
+        elif opt == '--originalFile':
+            originalFile = arg
+        elif opt == '--translatedFile':
+            translatedFile = arg
+    if englishFile == '' or originalFile == '' or translatedFile == '':
+        printUsage()
+        sys.exit(2)
+    print "Parsing files..."
+    encoding = "UTF-8"
+    englishXml = ET.parse(englishFile)
+    originalXml = ET.parse(originalFile)
+    translatedXml = ET.parse(translatedFile)
+    for stringElement in translatedXml.findall("string"):
+        xpathQuery = ".//string[@name='" + stringElement.get("name") + "']"
+        originalElement = originalXml.getroot().find(xpathQuery)
+        englishElement = englishXml.getroot().find(xpathQuery)
+        translatedValue = ET.tostring(stringElement, encoding, "text").strip()
+        englishValue = ET.tostring(englishElement, encoding, "text").strip()
+        originalValue = ET.tostring(originalElement, encoding, "text").strip()
+        if translatedValue != englishValue and translatedValue != originalValue:
+            originalElement.text = translatedValue
+    locFile = open(originalFile, "w")
+    locFile.write(ET.tostring(originalXml.getroot()))
+    print "Finished. ", originalFile, " has been merged with ", translatedFile
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
