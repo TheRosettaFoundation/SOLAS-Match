2146c855c82e3eee4244fa7c4e7910e078905316
TheIceMageX50 <palkiax50@topmail.ie>
2014-03-20 14:05:12 +0000

    Commiting fix to api/v0/Projects.php to correctly call ProjectDao::createUpdate(), not save() which is a private function

---
+++ test/ui/OrganisationDaoTest.php
@@ -0,0 +1,66 @@
+<?php
+namespace SolasMatch\Tests\UI;
+
+require_once 'PHPUnit/Autoload.php';
+require_once __DIR__.'/../../api/vendor/autoload.php';
+\DrSlump\Protobuf::autoload();
+require_once __DIR__.'/../../api/DataAccessObjects/AdminDao.class.php';
+require_once __DIR__.'/../../api/DataAccessObjects/BadgeDao.class.php';
+require_once __DIR__.'/../../api/DataAccessObjects/UserDao.class.php';
+require_once __DIR__.'/../../api/lib/Notify.class.php';
+require_once __DIR__.'/../../Common/Enums/BadgeTypes.class.php';
+require_once __DIR__.'/../../Common/Enums/HttpMethodEnum.class.php';
+require_once __DIR__.'/../../Common/lib/ModelFactory.class.php';
+require_once __DIR__.'/../../Common/lib/SolasMatchException.php';
+require_once __DIR__.'/../../Common/lib/UserSession.class.php';
+require_once __DIR__.'/../../ui/DataAccessObjects/BadgeDao.class.php';
+require_once __DIR__.'/../../ui/DataAccessObjects/OrganisationDao.class.php';
+require_once __DIR__.'/../../ui/DataAccessObjects/UserDao.class.php';
+require_once __DIR__.'/../../ui/lib/Localisation.php';
+require_once __DIR__.'/../UnitTestHelper.php';
+
+use \SolasMatch\API as API;
+use \SolasMatch\Common as Common;
+use \SolasMatch\Tests as Tests;
+use \SolasMatch\UI as UI;
+use SolasMatch\Tests\UnitTestHelper;
+
+class OrganisationDaoTest extends \PHPUnit_Framework_TestCase
+{
+    public function testCreateOrg()
+    {
+        UnitTestHelper::teardownDb();
+    
+        $userDao = new UI\DAO\UserDao();
+        $orgDao = new UI\DAO\OrganisationDao();
+    
+        $userEmail = "blah@test.com";
+        $userPw = "password";
+        $isRegistered = $userDao->register($userEmail, $userPw);
+        $this->assertTrue($isRegistered);
+    
+        $registerUser = API\DAO\UserDao::getUser(null, $userEmail);
+        $userId = $registerUser->getId();
+        $this->assertNotNull($registerUser);
+        $this->assertInstanceOf("\SolasMatch\Common\Protobufs\Models\User", $registerUser);
+        //Use API DAO because UI one requires UUID which we cannot retrieve (it would be emailed to the user)
+        $finishRegResult = API\DAO\UserDao::finishRegistration($registerUser->getId());
+        $this->assertEquals("1", $finishRegResult);
+    
+        $org = UnitTestHelper::createOrg();
+        try {
+            $insertedOrg = $orgDao->createOrg($org, $userId);
+        } catch (Common\Exceptions\SolasMatchException $e) {
+            $error = $e->getMessage();
+            $this->assertEquals(
+                    "The Authorization header does not match the current user or the user does not ".
+                    "have permission to access the current resource",
+                    $error
+            );
+        }
+        $userDao->login($userEmail, $userPw);
+        $insertedOrg = $orgDao->createOrg($org, $userId);
+        $this->assertNotNull($insertedOrg);
+        $this->assertInstanceOf("\SolasMatch\Common\Protobufs\Models\Organisation", $insertedOrg);
+    }
+}
