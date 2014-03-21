42316ebef52ee439ca762edbbca4a2a23939ea8e
David O Carroll <spaceindaver0@gmail.com>
2014-03-21 12:04:57 +0000

    Undo Aaron merge mistake
    Update the StatDao on the API to have my changes that were removed

--- api/DataAccessObjects/StatDao.class.php
+++ api/DataAccessObjects/StatDao.class.php
@@ -5,12 +5,8 @@ namespace SolasMatch\API\DAO;
 use \SolasMatch\API\Lib as Lib;
 use \SolasMatch\Common as Common;
 
-/**
- * Description of StatDao
- *
- * @author sean
- */
 require_once __DIR__."/../../api/lib/PDOWrapper.class.php";
+
 //! Statistcs Data Access Object for getting site statistics in the API
 /*!
   The Statistics Data Access Object for retrieving data from the Database. It has direct Database access through the
@@ -20,6 +16,12 @@ require_once __DIR__."/../../api/lib/PDOWrapper.class.php";
 
 class StatDao
 {
+    //! Get a Statistic by name
+    /*!
+      Retrieve a statistic from the Database by name. If a name is not passed then all statistics are returned.
+      @param string $name is the name of the requested statistic or null for all statistics
+      @return Returns a list of Statistic objects or null
+    */
     public static function getStatistics($name)
     {
         $ret = null;
@@ -34,61 +36,148 @@ class StatDao
         return $ret;
     }
     
+    //! Update the Archived Projects Statistic in the Database
+    /*!
+      Recalculate the Archived Projects Statistic. <b>Note:</b> This is a direct call to the Database and occurs
+      synchronously.
+      @return No return
+    */
     public static function updateArchivedProjects()
     {
         Lib\PDOWrapper::call("statsUpdateArchivedProjects", "");
     }
     
+    //! Update the Archived Tasks Statistic in the Database
+    /*!
+      Recalculate the Archived Tasks Statistic. <b>Note:</b> This is a direct call to the Database and occurs
+      synchronously.
+      @return No return
+    */
     public static function updateArchivedTasks()
     {
         Lib\PDOWrapper::call("statsUpdateArchivedTasks", "");
     }
     
+    //! Update the Badge count Statistic in the Database
+    /*!
+      Recalculate the Badge count Statistic. <b>Note:</b> This is a direct call to the Database and occurs
+      synchronously.
+      @return No return
+    */
     public static function updateBadges()
     {
         Lib\PDOWrapper::call("statsUpdateBadges", "");
     }
     
+    //! Update the claimed Tasks Statistic in the Database
+    /*!
+      Recalculate the claimed Tasks Statistic. <b>Note:</b> This is a direct call to the Database and occurs
+      synchronously.
+      @return No return
+    */
     public static function updateClaimedTasks()
     {
         Lib\PDOWrapper::call("statsUpdateClaimedTasks", "");
     }
     
+    //! Get a count of the successful logins
+    /*!
+      Count the number of successful login attempts starting at startDate up to but not including endDate.
+      @param String $startDate is the date to start counting from in the format "YYYY-MM-DD HH:MM:SS"
+      @param String $endDate is the date to count up to in the format "YYYY-MM-DD HH:MM:SS"
+      @return Returns an int represting the number of successful login attempts between start and end dates provided.
+    */
+    public static function getLoginCount($startDate, $endDate)
+    {
+        $args = Lib\PDOWrapper::cleanseNullOrWrapStr($startDate).', '.
+            Lib\PDOWrapper::cleanseNullOrWrapStr($endDate);
+        $result = Lib\PDOWrapper::call('getLoginCount', $args);
+        return $result[0]['result'];
+    }
+
+    //! Update the Organisation count Statistic in the Database
+    /*!
+      Recalculate the Organisation count Statistic. <b>Note:</b> This is a direct call to the Database and occurs
+      synchronously.
+      @return No return
+    */
     public static function updateOrganisations()
     {
         Lib\PDOWrapper::call("statsUpdateOrganisations", "");
     }
     
+    //! Update the Organisation membership requests Statistic in the Database
+    /*!
+      Recalculate the Organisation membership requests Statistic. <b>Note:</b> This is a direct call to the Database and
+      occurs synchronously.
+      @return No return
+    */
     public static function updateOrgMemberRequests()
     {
         Lib\PDOWrapper::call("statsUpdateOrgMemberRequests", "");
     }
     
+    //! Update the Project count Statistic in the Database
+    /*!
+      Recalculate the Project count Statistic. <b>Note:</b> This is a direct call to the Database and occurs
+      synchronously.
+      @return No return
+    */
     public static function updateProjects()
     {
         Lib\PDOWrapper::call("statsUpdateProjects", "");
     }
     
+    //! Update the Tag count Statistic in the Database
+    /*!
+      Recalculate the Tag count Statistic. <b>Note:</b> This is a direct call to the Database and occurs
+      synchronously.
+      @return No return
+    */
     public static function updateTags()
     {
         Lib\PDOWrapper::call("statsUpdateTags", "");
     }
     
+    //! Update the Task count Statistic in the Database
+    /*!
+      Recalculate the Task count Statistic. <b>Note:</b> This is a direct call to the Database and occurs
+      synchronously.
+      @return No return
+    */
     public static function updateTasks()
     {
         Lib\PDOWrapper::call("statsUpdateTasks", "");
     }
     
+    //! Update the Task with prerequisites count Statistic in the Database
+    /*!
+      Recalculate the Tasks with prerequisites count Statistic. <b>Note:</b> This is a direct call to the Database and
+      occurs synchronously.
+      @return No return
+    */
     public static function updateTasksWithPreReqs()
     {
         Lib\PDOWrapper::call("statsUpdateTasksWithPreReqs", "");
     }
     
+    //! Update the Unclaimed Tasks Statistic in the Database
+    /*!
+      Recalculate the Unclaimed Tasks Statistic. <b>Note:</b> This is a direct call to the Database and occurs
+      synchronously.
+      @return No return
+    */
     public static function updateUnclaimedTasks()
     {
         Lib\PDOWrapper::call("statsUpdateUnclaimedTasks", "");
     }
     
+    //! Update the User count Statistic in the Database
+    /*!
+      Recalculate the User count Statistic. <b>Note:</b> This is a direct call to the Database and occurs
+      synchronously.
+      @return No return
+    */
     public static function updateUsers()
     {
         Lib\PDOWrapper::call("statsUpdateUsers", "");
