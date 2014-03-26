fadb45145dcffb95a41d53101de4c0ba6a784f9c
David O Carroll <spaceindaver0@gmail.com>
2014-03-26 12:21:59 +0000

    Update Langs to use Slim for route handling and add groups

--- api/v0/Langs.php
+++ api/v0/Langs.php
@@ -6,97 +6,101 @@ use \SolasMatch\Common as Common;
 use \SolasMatch\API\Lib as Lib;
 use \SolasMatch\API as API;
 
-/**
- * Description of Languages
- *
- * @author sean
- */
-
 require_once __DIR__.'/../lib/Languages.class.php';
 
 class Langs
 {
     public static function init()
     {
-        API\Dispatcher::registerNamed(
-            Common\Enums\HttpMethodEnum::GET,
-            '/v0/languages(:format)/',
-            function ($format = ".json") {
-                $data = Lib\Languages::getLanguageList();
-                $result = null;
-                API\Dispatcher::sendResponse(null, $data, null, $format);
-            },
-            'getLanguages',
-            null
-        );
-            
-        API\Dispatcher::registerNamed(
-            Common\Enums\HttpMethodEnum::GET,
-            '/v0/languages/getActiveLanguages(:format)/',
-            function ($format = '.json') {
-                API\Dispatcher::sendResponse(null, Lib\Languages::getActiveLanguages(), null, $format);
-            },
-            'getActiveLanguages',
-            null
-        );
-                        
-        API\Dispatcher::registerNamed(
-            Common\Enums\HttpMethodEnum::GET,
-            '/v0/languages/getActiveSourceLanguages(:format)/',
-            function ($format = '.json') {
-                API\Dispatcher::sendResponse(null, Lib\Languages::getActiveSourceLanguages(), null, $format);
-            },
-            'getActiveSourceLanguages',
-            null
-        );
-          
-        API\Dispatcher::registerNamed(
-            Common\Enums\HttpMethodEnum::GET,
-            '/v0/languages/getActiveTargetLanguages(:format)/',
-            function ($format = '.json') {
-                API\Dispatcher::sendResponse(null, Lib\Languages::getActiveTargetLanguages(), null, $format);
-            },
-            'getActiveTargetLanguages',
-            null
-        );
-      
-        API\Dispatcher::registerNamed(
-            Common\Enums\HttpMethodEnum::GET,
-            '/v0/languages/:languageId/',
-            function ($languageId, $format = ".json") {
-                if (!is_numeric($languageId) && strstr($languageId, '.')) {
-                    $languageId = explode('.', $languageId);
-                    $format = '.'.$languageId[1];
-                    $languageId = $languageId[0];
-                }
-                $data = Lib\Languages::getLanguage($languageId, null, null);
-                if (is_array($data)) {
-                    $data = $data[0];
-                }
-                API\Dispatcher::sendResponse(null, $data, null, $format);
-            },
-            'getLanguage',
-            null
-        );
-        
-        API\Dispatcher::registerNamed(
-            Common\Enums\HttpMethodEnum::GET,
-            '/v0/languages/getByCode/:code/',
-            function ($code, $format = ".json") {
-                if (!is_numeric($code) && strstr($code, '.')) {
-                    $code = explode('.', $code);
-                    $format = '.'.$code[1];
-                    $code = $code[0];
-                }
-                $data = Lib\Languages::getLanguage(null, $code, null);
-                if (is_array($data) && is_array($data[0])) {
-                    $data = $data[0];
-                }
-                API\Dispatcher::sendResponse(null, $data, null, $format);
-            },
-            'getLanguageByCode',
-            null
-        );
+        $app = \Slim\Slim::getInstance();
+
+        $app->group('/v0', function () use ($app) {
+            $app->group('/languages', function () use ($app) {
+
+                /* Routes starting /v0/languages */
+                $app->get(
+                    '/getActiveLanguages(:format)/',
+                    '\SolasMatch\API\V0\Langs::getActiveLanguages'
+                );
+
+                $app->get(
+                    '/getActiveSourceLanguages(:format)/',
+                    '\SolasMatch\API\V0\Langs::getActiveSourceLanguages'
+                );
+
+                $app->get(
+                    '/getActiveTargetLanguages(:format)/',
+                    '\SolasMatch\API\V0\Langs::getActiveTargetLanguages'
+                );
+
+                $app->get(
+                    '/getByCode/:code/',
+                    '\SolasMatch\API\V0\Langs::getLanguageByCode'
+                );
+
+                $app->get(
+                    '/:languageId/',
+                    '\SolasMatch\API\V0\Langs::getLanguage'
+                );
+            });
+
+            /* Routes starting /v0 */
+            $app->get(
+                '/languages(:format)/',
+                '\SolasMatch\API\V0\Langs::getLanguages'
+            );
+        });
+    }
+
+    public static function getActiveLanguages($format = '.json')
+    {
+        API\Dispatcher::sendResponse(null, Lib\Languages::getActiveLanguages(), null, $format);
+    }
+
+    public static function getActiveSourceLanguages($format = '.json')
+    {
+        API\Dispatcher::sendResponse(null, Lib\Languages::getActiveSourceLanguages(), null, $format);
+    }
+
+    public static function getActiveTargetLanguages($format = '.json')
+    {
+        API\Dispatcher::sendResponse(null, Lib\Languages::getActiveTargetLanguages(), null, $format);
+    }
+
+    public static function getLanguageByCode($code, $format = ".json")
+    {
+        if (!is_numeric($code) && strstr($code, '.')) {
+            $code = explode('.', $code);
+            $format = '.'.$code[1];
+            $code = $code[0];
+        }
+        $data = Lib\Languages::getLanguage(null, $code, null);
+        if (is_array($data) && is_array($data[0])) {
+            $data = $data[0];
+        }
+        API\Dispatcher::sendResponse(null, $data, null, $format);
+    }
+
+    public static function getLanguage($languageId, $format = ".json")
+    {
+        if (!is_numeric($languageId) && strstr($languageId, '.')) {
+            $languageId = explode('.', $languageId);
+            $format = '.'.$languageId[1];
+            $languageId = $languageId[0];
+        }
+        $data = Lib\Languages::getLanguage($languageId, null, null);
+        if (is_array($data)) {
+            $data = $data[0];
+        }
+        API\Dispatcher::sendResponse(null, $data, null, $format);
+    }
+
+    public static function getLanguages($format = ".json")
+    {
+        $data = Lib\Languages::getLanguageList();
+        $result = null;
+        API\Dispatcher::sendResponse(null, $data, null, $format);
     }
 }
+
 Langs::init();
