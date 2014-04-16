17c2074d40f2e4a8d86e38b4a83f689c133934ff
=Asanka Wasala <Asanka.Wasala@ul.ie>
2014-04-16 15:00:46 +0100

    Fixed validation issues.

--- ui/lib/Validator.php
+++ ui/lib/Validator.php
@@ -4,6 +4,14 @@ namespace SolasMatch\UI\Lib;
 
 class Validator
 {
+    
+    private static function addhttp($url) {
+        if (!preg_match("~^(?:f|ht)tps?://~i", $url)) {
+            $url = "http://" . $url;
+        }
+        return $url;
+    }
+    
     public static function filterSpecialChars($title)
     {
         //modify common_invalid_character in strings.xml to reflect  changes here
@@ -17,7 +25,7 @@ class Validator
     
     public static function validateURL($url)
     { 
-      return (filter_var($url, FILTER_VALIDATE_URL));
+      return (filter_var(self::addhttp($url), FILTER_VALIDATE_URL));
     }
     
     public static function validateEmail($url)
