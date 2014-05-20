aa2e10307fcfb8e201b8c24377a5825f17a4a152
Tadhg <tadhg.fla.work@gmail.com>
2014-05-20 15:20:11 +0100

    Fixed the position of Deadline jQuery calendar on project edit page.  Fixes #911

--- ui/js/datetime-picker.js
+++ ui/js/datetime-picker.js
@@ -1,9 +1,19 @@
 function initDeadlinePicker() {
+	
+	var position = $( "#deadline" ).position();
+
     $('#deadline').datetimepicker({
         timeFormat: "HH:mm 'UTC'",
         dateFormat: "d MM yy",
         stepHour: 1,
-        stepMinute: 10
+        stepMinute: 10,
+        beforeShow: function (input, inst) {
+        setTimeout(function () {
+            inst.dpDiv.css({
+                top: position.top+30
+                           });
+        }, 0);
+    }
     });
 }
 
