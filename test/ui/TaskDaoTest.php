8a751d832de7ae56cf1198ff3ce5b7647068b268
TheIceMageX50 <palkiax50@topmail.ie>
2014-05-21 15:36:10 +0100

    Added some more UI unit tests for tasks and projects. Part of #781

--- test/ui/TaskDaoTest.php
+++ test/ui/TaskDaoTest.php
@@ -4,7 +4,10 @@ namespace SolasMatch\Tests\UI;
 require_once 'PHPUnit/Autoload.php';
 require_once __DIR__.'/../../api/vendor/autoload.php';
 \DrSlump\Protobuf::autoload();
+require_once __DIR__.'/../../api/DataAccessObjects/BadgeDao.class.php';
+require_once __DIR__.'/../../api/DataAccessObjects/ProjectDao.class.php';
 require_once __DIR__.'/../../api/DataAccessObjects/UserDao.class.php';
+require_once __DIR__.'/../../api/DataAccessObjects/TagsDao.class.php';
 require_once __DIR__.'/../../api/lib/Notify.class.php';
 require_once __DIR__.'/../../Common/Enums/BadgeTypes.class.php';
 require_once __DIR__.'/../../Common/Enums/HttpMethodEnum.class.php';
@@ -67,5 +70,52 @@ class TaskDaoTest extends \PHPUnit_Framework_TestCase
         $insertedTask = $taskDao->createTask($task);
         $this->assertNotNull($insertedTask);
         $this->assertInstanceOf("\SolasMatch\Common\Protobufs\Models\Task", $insertedTask);
+    }
+    
+    /**
+     * @covers UI\DAO\TaskDao::getTask 
+     */
+    public function testGetTask()
+    {
+        UnitTestHelper::teardownDb();
+        
+        $userDao = new UI\DAO\UserDao();
+        $orgDao = new UI\DAO\OrganisationDao();
+        $projectDao = new UI\DAO\ProjectDao();
+        $taskDao = new UI\DAO\TaskDao();
+        
+        $userEmail = "blah@test.com";
+        $userPw = "password";
+        $isRegistered = $userDao->register($userEmail, $userPw);
+        $this->assertTrue($isRegistered);
+        
+        $registerUser = API\DAO\UserDao::getUser(null, $userEmail);
+        $userId = $registerUser->getId();
+        $this->assertNotNull($registerUser);
+        $this->assertInstanceOf("\SolasMatch\Common\Protobufs\Models\User", $registerUser);
+        //Use API DAO because UI one requires UUID which we cannot retrieve (it would be emailed to the user)
+        $finishRegResult = API\DAO\UserDao::finishRegistration($userId);
+        $this->assertEquals("1", $finishRegResult);
+        
+        $userDao->login($userEmail, $userPw);
+        $org = UnitTestHelper::createOrg();
+        $insertedOrg = $orgDao->createOrg($org, $userId);
+        $this->assertNotNull($insertedOrg);
+        $this->assertInstanceOf("\SolasMatch\Common\Protobufs\Models\Organisation", $insertedOrg);
+        
+        $project = UnitTestHelper::createProject($insertedOrg->getId());
+        $insertedProject = $projectDao->createProject($project);
+        $this->assertNotNull($insertedProject);
+        $this->assertInstanceOf("\SolasMatch\Common\Protobufs\Models\Project", $insertedProject);
+        
+        $task = UnitTestHelper::createTask($insertedProject->getId());
+        $insertedTask = $taskDao->createTask($task);
+        $this->assertNotNull($insertedTask);
+        $this->assertInstanceOf("\SolasMatch\Common\Protobufs\Models\Task", $insertedTask);
+        
+        $getTask = $taskDao->getTask($insertedTask->getId());
+        $this->assertNotNull($getTask);
+        $this->assertInstanceOf("\SolasMatch\Common\Protobufs\Models\Task", $getTask);
+        $this->assertEquals($insertedTask, $getTask);
     }
 }
