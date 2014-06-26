d60863b8338451462fb82341c6305b6c00b9c906
TheIceMageX50 <palkiax50@topmail.ie>
2014-06-26 16:53:04 +0100

    Added doc comments to some of the Dart DAOs. Part of #876

--- ui/dart/web/DataAccessObjects/CountryDao.dart
+++ ui/dart/web/DataAccessObjects/CountryDao.dart
@@ -1,7 +1,15 @@
 part of SolasMatchDart;
 
+/**
+ * Class containing methods to access [Country]-related data through the API.
+ */
 class CountryDao
 {
+  /**
+   * Calls the API to get a [List] of all Countries.
+   * 
+   * Returns a [Future] whose value will be a [List] of [Country] objects on successful completion.
+   */
   static Future<List<Country>> getAllCountries()
   {
     APIHelper client = new APIHelper(".json");
