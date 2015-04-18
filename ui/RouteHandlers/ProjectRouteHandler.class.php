774731172627fb05436d967ab338dc031c7de0a4
Alan Barrett <alanabarrett0@gmail.com>
2015-04-18 08:19:22 +0100

    Fix "conflicts" from merge [use develop branch]

--- ui/RouteHandlers/ProjectRouteHandler.class.php
+++ ui/RouteHandlers/ProjectRouteHandler.class.php
@@ -55,6 +55,7 @@ class ProjectRouteHandler
 
         $app->get(
             "/project/:project_id/file/",
+            array($middleware, "authUserIsLoggedIn"),
             array($this, "downloadProjectFile")
         )->name("download-project-file");
 
