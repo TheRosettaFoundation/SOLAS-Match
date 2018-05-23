1813b2b863f60f2c1ec96e1b780bac71bd426950
Alan Barrett <alanabarrett0@gmail.com>
2018-05-23 11:12:31 +0100

    SQL

--- db/schema.sql
+++ db/schema.sql
@@ -1472,6 +1472,13 @@ CREATE TABLE IF NOT EXISTS `TaskInviteSentToUsers` (
     CONSTRAINT FK_invite_user_id FOREIGN KEY (user_id) REFERENCES Users (id) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+CREATE TABLE IF NOT EXISTS `TermsAcceptedUsers` (
+    user_id        INT (10) UNSIGNED NOT NULL,
+    accepted_level INT (10) UNSIGNED NOT NULL,
+    KEY FK_terms_user_id (user_id),
+    CONSTRAINT FK_terms_user_id FOREIGN KEY (user_id) REFERENCES Users (id) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
 /*---------------------------------------end of tables---------------------------------------------*/
 
 /*---------------------------------------start of procs--------------------------------------------*/
