057cc989648ce1c381eb29a35a73aab5d96c6baa
Alan Barrett <alanabarrett0@gmail.com>
2018-11-09 17:10:49 +0000

    Take out class serialization tests

--- ui/RouteHandlers/ProjectRouteHandler.class.php
+++ ui/RouteHandlers/ProjectRouteHandler.class.php
@@ -500,47 +500,6 @@ class ProjectRouteHandler
             $project_tasks = $projectDao->getProjectTasks($project_id);
             $taskLanguageMap = array();
             if ($project_tasks) {
-require_once __DIR__."/../../Common/protobufs/notifications/TaskRevokedNotification.php";
-require_once __DIR__."/../../Common/protobufs/notifications/TaskRevokedNotificationx.php";
-$pj = new Common\Protobufs\Notifications\TaskRevokedNotification();
-$ser = serialize($pj);
-error_log("new TaskRevokedNotification(): " . preg_replace('/[\x00-\x1F\x7F]/u', '?', $ser));
-//error_log(print_r($pj, true));
-$pj = new Common\Protobufs\Notifications\TaskRevokedNotificatioX();
-$ser = serialize($pj);
-error_log("new TaskRevokedNotificatioX(): " . preg_replace('/[\x00-\x1F\x7F]/u', '?', $ser));
-
-//require_once __DIR__."/../../Common/protobufs/emails/UserBadgeAwardedEmail.php";
-//$pj = new Common\Protobufs\Emails\UserBadgeAwardedEmail();
-//$ser = serialize($pj);
-//error_log("new UserBadgeAwardedEmail(): " . preg_replace('/[\x00-\x1F\x7F]/u', '?', $ser));
-//error_log(print_r($pj, true));
-
-//$pj = new Common\Protobufs\Models\Project();
-//$ser = serialize($pj);
-//error_log("new Project(): " . preg_replace('/[\x00-\x1F\x7F]/u', '?', $ser));
-//error_log(print_r($pj, true));
-//error_log(print_r(unserialize($ser), true));
-//
-//$pj->setImpact(1);
-//$pj->setImageUploaded(1);
-//$pj->setImageApproved('1');
-//$pj->setOrganisationId('2a2');
-//$pj->setId('99');
-//$pj->setId(555);
-//$pj->setDeadline(555);
-////$pj->setTitle(NULL);
-//$ser = serialize($pj);
-//error_log("Project After sets: " . preg_replace('/[\x00-\x1F\x7F]/u', '?', $ser));
-//
-//$ser = serialize($project);
-//error_log("Project: " . preg_replace('/[\x00-\x1F\x7F]/u', '?', $ser));
-//error_log(print_r($project, true));
-//error_log(print_r(unserialize($ser), true));
-                foreach ($project_tasks as $task) {
-//$ser = serialize($task);
-//error_log("Task: " . preg_replace('/[\x00-\x1F\x7F]/u', '?', $ser));
-//error_log(print_r($task, true));
                     $targetLocale = $task->getTargetLocale();
                     $taskTargetLanguage = $targetLocale->getLanguageCode();
                     $taskTargetCountry = $targetLocale->getCountryCode();
@@ -559,10 +518,6 @@ error_log("new TaskRevokedNotificatioX(): " . preg_replace('/[\x00-\x1F\x7F]/u',
             }
 
             $graph = $projectDao->getProjectGraph($project_id);
-//$ser = serialize($graph);
-//error_log("WorkflowGraph: " . preg_replace('/[\x00-\x1F\x7F]/u', '?', $ser));
-//error_log(print_r($graph, true));
-//error_log(print_r(unserialize($ser), true));
             $viewer = new Lib\GraphViewer($graph);
             $graphView = $viewer->constructView();
 
