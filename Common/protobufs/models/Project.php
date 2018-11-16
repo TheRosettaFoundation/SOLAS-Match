6c5be53f36e86cafe47fbe10bc3cb5572473979f
Alan Barrett <alanabarrett0@gmail.com>
2018-11-16 11:43:26 +0000

    Take out debug

--- Common/protobufs/models/Project.php
+++ Common/protobufs/models/Project.php
@@ -153,7 +153,6 @@ class Project
   }
 
   public function setImageUploaded($imageUploaded) {
-if ($imageUploaded) error_log("imageUploaded TRUE, $imageUploaded"); else error_log("imageUploaded FALSE, $imageUploaded");
     $this->imageUploaded = (boolean)$imageUploaded;
   }
 
