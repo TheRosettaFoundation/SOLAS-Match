61aff3cbcd2ae2e520e7db10c535508d2ac52d13
Alan Barrett <alanabarrett0@gmail.com>
2018-11-16 15:20:36 +0000

    Strip trailing M or whatever

--- ui/lib/TemplateHelper.php
+++ ui/lib/TemplateHelper.php
@@ -324,19 +324,22 @@ class TemplateHelper
     public static function maxFileSizeBytes()
     {
         $display_max_size = self::maxUploadSizeFromPHPSettings();
+        $bytes = substr($display_max_size, 0, -1);
+error_log("bytes $bytes;");
 
         switch (substr($display_max_size, -1)) {
             case "G":   // no break
-                $display_max_size = $display_max_size * 1024;
+                $bytes = $bytes * 1024;
                 // no break
             case "M":
-                $display_max_size = $display_max_size * 1024;
+                $bytes = $bytes * 1024;
                 // no break
             case "K":
-                $display_max_size = $display_max_size * 1024;
+                $bytes = $bytes * 1024;
                 break;
         }
-        return $display_max_size;
+error_log("bytes $bytes;");
+        return $bytes;
     }
 
     /**
