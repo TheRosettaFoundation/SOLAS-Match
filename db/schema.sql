6834830fbf6634831d3b9fb62780b699eb9e2efa
Alan Barrett <alanabarrett0@gmail.com>
2019-03-27 14:27:17 +0000

    DiscourseID Table

--- db/schema.sql
+++ db/schema.sql
@@ -1534,6 +1534,13 @@ CREATE TABLE IF NOT EXISTS `TaskCompleteDates` (
   CONSTRAINT `FK_TaskCompleteDates_task_id` FOREIGN KEY (`task_id`) REFERENCES `Tasks` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+CREATE TABLE IF NOT EXISTS `DiscourseID` (
+  project_id INT(10) UNSIGNED NOT NULL,
+  topic_id   INT(10) UNSIGNED NOT NULL,
+  PRIMARY KEY (project_id),
+  CONSTRAINT FK_DiscourseID_project_id FOREIGN KEY (project_id) REFERENCES Projects (id) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
 /*---------------------------------------end of tables---------------------------------------------*/
 
 /*---------------------------------------start of procs--------------------------------------------*/
