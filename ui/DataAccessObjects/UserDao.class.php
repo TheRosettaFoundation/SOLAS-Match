82ec96013724d234c52dcf9393dfd2b0ab2c0dbf
Alan Barrett <alanabarrett0@gmail.com>
2019-05-24 15:31:03 +0100

    Take out debug

--- ui/DataAccessObjects/UserDao.class.php
+++ ui/DataAccessObjects/UserDao.class.php
@@ -32,7 +32,6 @@ class UserDao extends BaseDao
     
     public function getUser($userId)
     {
-error_log("user_id: $userId");
         $ret = null;
         
         $ret = Common\Lib\CacheHelper::getCached(
@@ -41,8 +40,6 @@ error_log("user_id: $userId");
             function ($args) {
                 $user = null;
                 $result = LibAPI\PDOWrapper::call('getUser', LibAPI\PDOWrapper::cleanseNull($args[0]) . ',null,null,null,null,null,null,null,null');
-error_log('result');
-error_log(print_r($result, true));
                 if (!empty($result)) {
                     $user = Common\Lib\ModelFactory::buildModel('User', $result[0]);
                     if (!is_null($user)) {
@@ -50,14 +47,10 @@ error_log(print_r($result, true));
                         $user->setNonce('');
                     }
                 }
-error_log('user');
-error_log(print_r($user, true));
                 return $user;
             },
             array($userId)
         );
-error_log('ret');
-error_log(print_r($ret, true));
         return $ret;
     }
     
