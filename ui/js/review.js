a89218fa71b04fbd93dc8e451bbb59e1598aa2ec
Alan Barrett <alanabarrett0@gmail.com>
2019-09-02 19:24:04 +0100

    Both types supported

--- ui/js/review.js
+++ ui/js/review.js
@@ -4,21 +4,41 @@
     {
         for (var i = 0; i < taskIds.length; i++) {
             var form = $("#TaskReviewForm");
-            var corrRate = $("#rateit_accuracy_" + taskIds[i].toString());
-            var corrections = $("<input type=\"hidden\" name=\"corrections_" + taskIds[i] + "\" />").attr("value",
-                    corrRate.rateit('value'));
+
+            var corrRate;
+            try {
+                corrRate = $("#rateit_corrections_" + taskIds[i].toString()).rateit('value');
+            } catch (err) {
+                corrRate = $("#rateit_accuracy_"    + taskIds[i].toString()).rateit('value');
+            }
+            var corrections = $("<input type=\"hidden\" name=\"corrections_" + taskIds[i] + "\" />").attr("value", corrRate);
             form.append(corrections);
 
-            var grammar = $("<input type=\"hidden\" name=\"grammar_" + taskIds[i] + "\" />").attr("value",
-                    $("#rateit_fluency_" + taskIds[i]).rateit('value'));
+            var gramRate;
+            try {
+                gramRate = $("#rateit_grammar_" + taskIds[i].toString()).rateit('value');
+            } catch (err) {
+                gramRate = $("#rateit_fluency_" + taskIds[i].toString()).rateit('value');
+            }
+            var grammar = $("<input type=\"hidden\" name=\"grammar_" + taskIds[i] + "\" />").attr("value", gramRate);
             form.append(grammar);
 
-            var spelling = $("<input type=\"hidden\" name=\"spelling_" + taskIds[i] + "\" />").attr("value",
-                    $("#rateit_terminology_" + taskIds[i]).rateit('value'));
+            var spellRate;
+            try {
+                spellRate = $("#rateit_spelling_"    + taskIds[i].toString()).rateit('value');
+            } catch (err) {
+                spellRate = $("#rateit_terminology_" + taskIds[i].toString()).rateit('value');
+            }
+            var spelling = $("<input type=\"hidden\" name=\"spelling_" + taskIds[i] + "\" />").attr("value", spellRate);
             form.append(spelling);
 
-            var consistency = $("<input type=\"hidden\" name=\"consistency_" + taskIds[i] + "\"/>").attr("value",
-                    parseInt($("#rateit_style_" + taskIds[i]).rateit('value')) + 10*parseInt($("#rateit_design_" + taskIds[i]).rateit('value')));
+            var consRate;
+            try {
+                consRate = $("#rateit_consistency_"    + taskIds[i].toString()).rateit('value');
+            } catch (err) {
+                consRate = parseInt($("#rateit_style_" + taskIds[i].toString()).rateit('value')) + 10*parseInt($("#rateit_design_" + taskIds[i].toString()).rateit('value'));
+            }
+            var consistency = $("<input type=\"hidden\" name=\"consistency_" + taskIds[i] + "\" />").attr("value", consRate);
             form.append(consistency);
         }
     }
