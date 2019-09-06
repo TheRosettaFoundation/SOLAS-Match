5b2d7591d7eeb3c022557a8e1765ed57f59d592d
Alan Barrett <alanabarrett0@gmail.com>
2019-09-06 12:09:19 +0100

    Replace

--- api/DataAccessObjects/ProjectDao.class.php
+++ api/DataAccessObjects/ProjectDao.class.php
@@ -247,7 +247,7 @@ error_log('Project: ' . print_r(ProjectDao::getProject($projectId), true));
 error_log('ArchivedProject: ' . print_r(ProjectDao::getArchivedProject($projectId), true));
 error_log('Tasks: ' . print_r(ProjectDao::getProjectTasks($projectId), true));
 error_log('ArchivedTasks: ' . print_r(ProjectDao::getArchivedTask($projectId), true));
-//        $result = Lib\PDOWrapper::call("archiveProject", $args);
+        $result = Lib\PDOWrapper::call("archiveProject", $args);
 error_log('Project: ' . print_r(ProjectDao::getProject($projectId), true));
 error_log('ArchivedProject: ' . print_r(ProjectDao::getArchivedProject($projectId), true));
 error_log('Tasks: ' . print_r(ProjectDao::getProjectTasks($projectId), true));
