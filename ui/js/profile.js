5ec8a30f21d6685f90e8d5642e6d6cc497a6ac9e
Alan Barrett <alanabarrett0@gmail.com>
2020-06-23 12:14:14 +0100

    Each needs to call a function

--- ui/js/profile.js
+++ ui/js/profile.js
@@ -4,31 +4,34 @@ $(document).ready(documentReady);
 
 function documentReady()
 {
-  $(".add_click_handler").each.click(function(e) {
-      $("#dialog_for_verification").dialog(
-		{
-          resizable: false,
-          height:    "auto",
-          width:     400,
-          modal:     true,
-          buttons: {
-
-            "OK": function()
+  $(".add_click_handler").each(function ()
+    {
+      $(this).onclick = function(e)
+        {
+          $("#dialog_for_verification").dialog(
             {
-              e.target.parentNode.submit();
-            },
+              resizable: false,
+              height:    "auto",
+              width:     400,
+              modal:     true,
+              buttons: {
 
-            "Cancel": function()
-            {
-              $(this).dialog("close");
+                "OK": function()
+                {
+                  e.target.parentNode.submit();
+                },
+
+                "Cancel": function()
+                {
+                  $(this).dialog("close");
+                }
+              }
             }
-          }
-        }
-      );
+          );
 
-      e.preventDefault();
-      return false;
+          e.preventDefault();
+          return false;
+      }
     }
-  );
 }
 </script>
