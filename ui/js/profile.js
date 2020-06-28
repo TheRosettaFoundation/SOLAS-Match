82222ac8ba02c0e3d8d8779120a0ed82f8544595
Alan Barrett <alanabarrett0@gmail.com>
2020-06-28 17:18:25 +0100

    Takeout test code

--- ui/js/profile.js
+++ ui/js/profile.js
@@ -4,10 +4,8 @@ function documentReady()
 {
   $(".add_click_handler").each(function ()
     {
-//alert("before adding click handler for: " + $(this).getAttribute("name"));
       $(this).click(function(e)
           {
-alert("Handler for .click() called");
             $("#dialog_for_verification").dialog(
               {
                 resizable: false,
@@ -18,20 +16,17 @@ alert("Handler for .click() called");
 
                   "OK": function()
                   {
-alert("before trying submit of parent, action: " +   e.target.parentNode.action);
-//                    e.target.parentNode.submit();
+                    //alert("action: " + e.target.parentNode.action);
+                    e.target.parentNode.submit();
                   },
 
                   "Cancel": function()
                   {
-alert("before trying close dialog");
-
                     $(this).dialog("close");
                   }
                 }
               }
             );
-alert("after dialog added");
 
             e.preventDefault();
             return false;
