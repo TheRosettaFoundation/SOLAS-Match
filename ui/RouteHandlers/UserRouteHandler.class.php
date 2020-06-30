d6fb2a9297a485ecde411edc4d5de82a7ec6950e
Alan Barrett <alanabarrett0@gmail.com>
2020-06-30 11:20:57 +0100

    Wording

--- ui/RouteHandlers/UserRouteHandler.class.php
+++ ui/RouteHandlers/UserRouteHandler.class.php
@@ -1719,7 +1719,7 @@ $project->setOrganisationId(380);//DEV SERVER
                             curl_close($re);
 
                             $app->flashNow('success', '<a href="' . $app->urlFor('task-view', ['task_id' => $translation_task_id]) .
-                            '">This is your Verification task</a>, which you <strong>must</strong> translate using Kató TM. You will find the <strong>Translate using Kató TM button</strong> under the Verification task in your <strong>Claimed Tasks</strong> section, which you can find in the upper menu. You will need to refresh that page after a few minutes in order to see the button. Please, check your email inbox in a few minutes for instructions');
+                            '">This is your Translation Test</a>, which you <strong>must</strong> translate using Kató TM. You will find the <strong>Translate using Kató TM</strong> button under the Translation Test task in your <strong>Claimed Tasks</strong> section, which you can find in the upper menu. You will need to refresh that page after a few minutes in order to see the button. Please check your email inbox in a few minutes for instructions on completing the test');
                         }
                     }
                 }
