f40b2de27ff4cb9f3e7b9e446571ccd0559f9dac
Alan Barrett <alanabarrett0@gmail.com>
2020-11-22 16:47:36 +0000

    debug

--- ui/DataAccessObjects/ProjectDao.class.php
+++ ui/DataAccessObjects/ProjectDao.class.php
@@ -746,6 +746,7 @@ $memsource_change_country_to_kp = [
     public function queue_copy_task_original_file($project_id, $task_id, $memsource_task_uid, $filename)
     {
 error_log('here q');
+/*
 error_log(LibAPI\PDOWrapper::cleanse($project_id) . ',' .
             LibAPI\PDOWrapper::cleanse($task_id) . ',' .
             LibAPI\PDOWrapper::cleanseWrapStr($memsource_task_uid) . ',' .
@@ -755,6 +756,7 @@ error_log(LibAPI\PDOWrapper::cleanse($project_id) . ',' .
             LibAPI\PDOWrapper::cleanse($task_id) . ',' .
             LibAPI\PDOWrapper::cleanseWrapStr($memsource_task_uid) . ',' .
             LibAPI\PDOWrapper::cleanseWrapStr($filename));
+*/
 error_log('here q END');
     }
 
