7a3838a24e92596cd074628d62902fbef1e08a0c
Alan Barrett <alanabarrett0@gmail.com>
2020-11-26 11:40:26 +0000

    debug

--- ui/DataAccessObjects/ProjectDao.class.php
+++ ui/DataAccessObjects/ProjectDao.class.php
@@ -650,8 +650,8 @@ error_log($uploadFolder);//(**)
             $previous_path = "files/proj-$project_id/task-$min_id/v-0/$filename";
 error_log($previous_path);//(**)
             $previous_file = '';
-                if (file_exists(Common\Lib\Settings::get('files.upload_path') . $previous_path)) {
-                    $previous_file = file_get_contents(Common\Lib\Settings::get('files.upload_path') . $previous_path);
+            if (file_exists(Common\Lib\Settings::get('files.upload_path') . $previous_path)) {
+                $previous_file = file_get_contents(Common\Lib\Settings::get('files.upload_path') . $previous_path);
             }
 error_log("strlen(previous_file): " . strlen($previous_file));//(**)
 error_log("strlen(new file): " . strlen($file));//(**)
