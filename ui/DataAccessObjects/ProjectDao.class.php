f8a463c4a067c6a362bbe5a0d7d329985cf984da
Alan Barrett <alanabarrett0@gmail.com>
2021-01-16 15:50:26 +0000

    Take out test

--- ui/DataAccessObjects/ProjectDao.class.php
+++ ui/DataAccessObjects/ProjectDao.class.php
@@ -448,12 +448,9 @@ $replace = array(
             $language_options[$key] = $language;
         }
 
-error_log("create_memsource: $create_memsource");
-error_log($language_options['xen---']);
         if ($create_memsource) {
             $remove_memsource = ['xen---', 'aig-AG', 'bba-BJ', 'bah-BS', 'bjs-BB', 'ban-ID', 'bho-IN', 'gax-KE', 'vmw-MZ', 'chw-MZ', 'zdj-KM', 'pov-GW', 'dag-GH', 'grt-IN', 'gil-FJ', 'gcl-GD', 'gyn-GY', 'haw-US', 'nix-CD', 'ijc-NG', 'jam-JM', 'kab-DZ', 'kha-IN', 'kye-GH', 'led-CD', 'luy-KE', 'cma-KE', 'mgh-MZ', 'xsq-MZ', 'kde-MZ', 'mni-IN', 'men-SL', 'mer-KE', 'lus-IN', 'mfe-MU', 'wmw-MZ', 'ndc-MZ', 'ory-IN', 'pko-KE', 'acf-LC', 'saq-KE', 'srr-SN', 'crs-SC', 'srn-SR', 'tmh-DZ', 'twx-MZ', 'tsc-MZ', 'tuv-KE', 'svc-VC', 'vic-US'];
             foreach ($remove_memsource as $remove) {
-error_log("Remove: $remove");
                 unset($language_options[$remove]);
             }
         }
