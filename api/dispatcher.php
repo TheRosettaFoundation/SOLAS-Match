e67e534ed9d8e3e69c6125e6b06af9c9a541c880
Alan Barrett <alanabarrett0@gmail.com>
2021-12-08 11:25:25 +0000

    debug

--- api/dispatcher.php
+++ api/dispatcher.php
@@ -69,6 +69,7 @@ class Dispatcher
     public static function init()
     {
         $path = self::getDispatcher()->request()->getResourceUri();
+error_log("function init() path: $path");//(**)
         $path = explode("/", $path);
         $path = $path[1];
         $providerNames = self::readProviders("$path/");
