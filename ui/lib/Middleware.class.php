1e57b2ced3716eee23f7855a7e03629948d53ab0
Alan Barrett <alanabarrett0@gmail.com>
2022-02-16 16:57:06 +0000

    Check

--- ui/lib/Middleware.class.php
+++ ui/lib/Middleware.class.php
@@ -14,6 +14,7 @@ class Middleware
 {
     public function SessionCookie(Request $request, RequestHandler $handler)
     {
+$aaaa = $secret;
         if (session_id() === '') {
             session_start();
         }
