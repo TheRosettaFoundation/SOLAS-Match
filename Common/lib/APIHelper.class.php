6ceacd6357765d9dc27f20fc60bf30bf0de708ae
Alan Barrett <alanabarrett0@gmail.com>
2022-02-17 10:39:43 +0000

    Take out unused

--- Common/lib/APIHelper.class.php
+++ Common/lib/APIHelper.class.php
@@ -7,10 +7,6 @@ use \SolasMatch\Common\Exceptions as Exceptions;
 
 require_once __DIR__."/../Enums/FormatEnum.class.php";
 require_once __DIR__."/JSONSerializer.class.php";
-//require_once __DIR__."/XMLSerializer.class.php";
-//require_once __DIR__."/HTMLSerializer.class.php";
-//require_once __DIR__."/PHPSerializer.class.php";
-//require_once __DIR__."/ProtobufSerializer.class.php";
 
 class APIHelper
 {
@@ -21,27 +17,7 @@ class APIHelper
 
     public function __construct($format)
     {
-        $format = self::getFormatFromString($format);
-
-        switch ($format)
-        {
-            default:
-            case Enums\FormatEnum::JSON:
-                $this->serializer = new JSONSerializer();
-                break;
-//            case Enums\FormatEnum::XML:
-//                $this->serializer = new XMLSerializer();
-//                break;
-//            case Enums\FormatEnum::HTML:
-//                $this->serializer = new HTMLSerializer();
-//                break;
-//            case Enums\FormatEnum::PHP:
-//                $this->serializer = new PHPSerializer();
-//                break;
-//            case Enums\FormatEnum::PROTOBUFS:
-//                $this->serializer = new ProtobufSerializer();
-//                break;
-        }
+        $this->serializer = new JSONSerializer();
     }
     
     public function call(
@@ -123,103 +99,6 @@ class APIHelper
         return $response_data;
     }
 
-    public function externalCall( //this function is mainly being used in Google+ Sign In calls
-        $destination,
-        $url,
-        $method = Enums\HttpMethodEnum::GET,
-        $data = null,
-        $query_args = array(),
-        $file = null,
-        $authorization_header = null,
-        $headers = array()
-    ) {
-        $url = $url."/?";
-        if (!empty($query_args) && count($query_args) > 0) {
-            $first = true;
-            foreach ($query_args as $key => $val) {
-                if (!$first) {
-                    $url .= "&";
-                } else {
-                    $first = false;
-                }
-                $url .= "$key=$val";
-            }
-        }
-        
-        $re = curl_init($url);
-        curl_setopt($re, CURLOPT_CUSTOMREQUEST, $method);
-        $length = 0;
-        if (!is_null($data) && "null" != $data) {
-            $data = $this->serializer->serialize($data);
-            curl_setopt($re, CURLOPT_POSTFIELDS, $data);
-            $length = strlen($data);
-        }
-        
-        if (!is_null($file)) {
-            $length = strlen($file);
-            curl_setopt($re, CURLOPT_POSTFIELDS, $file);
-        }
-
-        curl_setopt($re, CURLOPT_COOKIESESSION, true);
-        if (isset($_COOKIE['slim_session'])) {
-            curl_setopt($re, CURLOPT_COOKIE, "slim_session=".urlencode($_COOKIE['slim_session']).";");
-        }
-        
-        curl_setopt($re, CURLOPT_AUTOREFERER, true);
-        $token = UserSession::getAccessToken();
-        
-        $httpHeaders = array(
-            $this->serializer->getContentType(),
-            'Expect:',
-            'Content-Length:'.$length
-        );
-        if (!is_null($authorization_header)) {
-            $httpHeaders[] = 'Authorization: Bearer '.$authorization_header;
-        }
-        if (self::$UNIT_TESTING) {
-            $headers[] = 'X-UNIT-TESTING: 1';
-        }
-        if (!is_null($headers)) {
-            $httpHeaders = array_merge($httpHeaders, $headers);
-        }
-        curl_setopt($re, CURLOPT_HTTPHEADER, $httpHeaders);
-        curl_setopt($re, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($re, CURLOPT_HEADER, true);
-        $res = curl_exec($re);
-        $header_size = curl_getinfo($re, CURLINFO_HEADER_SIZE);
-        $header = substr($res, 0, $header_size);
-        $this->outputHeaders = http_parse_headers($header);
-        $res = substr($res, $header_size);
-        $success = array(200,201,202,203,204,301,303);
-        $this->responseCode = curl_getinfo($re, CURLINFO_HTTP_CODE);
-
-        curl_close($re);
-        if (in_array($this->responseCode, $success)) {
-            $response_data = $this->serializer->deserialize($res, $destination);
-        } else {
-            throw new Exceptions\SolasMatchException($res, $this->responseCode);
-        }
-        return $response_data;
-    }
-
-    public function cast($destination, $sourceObject)
-    {
-        $ret = null;
-        if (is_array($destination)) {
-            if ($sourceObject) {
-                foreach ($sourceObject as $row) {
-                    $ret[] = $this->serializer->cast($destination[0], $row);
-                }
-            }
-        } elseif (is_array($sourceObject)) {
-            $ret = $this->serializer->cast($destination, $sourceObject[0]);
-        } else {
-            $ret = $this->serializer->cast($destination, $sourceObject);
-        }
-
-        return $ret;
-    }
-
     public function serialize($data)
     {
         return $this->serializer->serialize($data);
@@ -228,24 +107,6 @@ class APIHelper
     public function deserialize($data, $type)
     {
         return $this->serializer->deserialize($data, $type);
-    }
-
-    public static function getFormatFromString($format)
-    {
-        if ($format == ".json") {
-            $format = Enums\FormatEnum::JSON;
-//        } elseif (strcasecmp($format, '.xml') == 0) {
-//            $format = Enums\FormatEnum::XML;
-//        } elseif (strcasecmp($format, '.php') == 0) {
-//            $format = Enums\FormatEnum::PHP;
-//        } elseif (strcasecmp($format, '.html') == 0) {
-//            $format = Enums\FormatEnum::HTML;
-//        } elseif (strcasecmp($format, '.proto') == 0) {
-//            $format = Enums\FormatEnum::PROTOBUFS;//change when implmented.
-        } else {
-            $format = Enums\FormatEnum::JSON;
-        }
-        return $format;
     }
 
     public static function parseFilterString($filter)
@@ -269,18 +130,17 @@ class APIHelper
         }
         return $ret;
     }
-    
+
     public function getResponseCode()
     {
         return $this->responseCode;
     }
-    
+
     public function getHeaders()
     {
         return $this->outputHeaders;
     }
-    
-    
+
     // http://stackoverflow.com/a/1147952
     private function systemExtensionMimeTypes()
     {
