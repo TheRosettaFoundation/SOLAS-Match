0547313739ef9b4b9390766c1d8283121413ec58
Alan Barrett <alanabarrett0@gmail.com>
2022-02-20 15:36:35 +0000

    /api

--- api/dispatcher.php
+++ api/dispatcher.php
@@ -33,16 +33,6 @@ require_once '/repo/SOLAS-Match/api/vendor/league/oauth2-server/src/League/OAuth
 require_once '/repo/SOLAS-Match/api/vendor/league/oauth2-server/src/League/OAuth2/Server/Util/SecureKey.php';
 require_once '/repo/SOLAS-Match/api/vendor/league/oauth2-server/src/League/OAuth2/Server/Util/RedirectUri.php';
 require_once '/repo/SOLAS-Match/api/vendor/league/oauth2-server/src/League/OAuth2/Server/Util/Request.php';
-$a12 = get_included_files();
-error_log(print_r($a12, true));
-$a12 = get_declared_classes();
-error_log(print_r($a12, true));
-foreach($a12 as $b12) {
-    if (strpos($b12, 'League') !== false) error_log("Class: $b12");
-    if (strpos($b12, 'OAuth2') !== false) error_log("Class: $b12");
-    if (strpos($b12, 'Storage') !== false) error_log("Class: $b12");
-    if (strpos($b12, 'ClientInterface') !== false) error_log("Class: $b12");
-}
 require_once __DIR__ . '/OAuth2/Client.php';
 require_once __DIR__ . '/OAuth2/Scope.php';
 require_once __DIR__ . '/OAuth2/Session.php';
