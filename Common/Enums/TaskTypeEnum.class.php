6dd19032f02a981b667d36c565bbf912bd177d28
Alan Barrett <alanabarrett0@gmail.com>
2022-09-20 18:33:31 +0100

    WIP

--- Common/Enums/TaskTypeEnum.class.php
+++ Common/Enums/TaskTypeEnum.class.php
@@ -10,6 +10,15 @@ class TaskTypeEnum
     const DESEGMENTATION    = 4;
     const QUALITY           = 5;
     const APPROVAL          = 6;
+
+    static $enum_to_UI = [
+        TaskTypeEnum::SEGMENTATION   => ['text' => 'Segmentation', 'colour' => '#B02323'],
+        TaskTypeEnum::TRANSLATION    => ['text' => 'Translation', 'colour' => '#1D8A11'],
+        TaskTypeEnum::PROOFREADING   => ['text' => 'Revising', 'colour' => '#1064C4'],
+        TaskTypeEnum::DESEGMENTATION => ['text' => 'Desegmentation', 'colour' => '#B02060'],
+        TaskTypeEnum::QUALITY        => ['text' => 'QA??', 'colour' => '#B02323'],
+        TaskTypeEnum::APPROVAL       => ['text' => 'Approval??', 'colour' => '#B02060'],
+    ];
 }
 
 $task_type_to_enum = [
@@ -17,13 +26,4 @@ $task_type_to_enum = [
     'Revision'                    => TaskTypeEnum::PROOFREADING,
     //(**)LexiQA not in Memsource: Language Quality Inspection' => TaskTypeEnum::QUALITY,
     'Proofreading and Approval'   => TaskTypeEnum::APPROVAL,
-];
-
-$enum_to_UI = [
-    TaskTypeEnum::SEGMENTATION   => ['text' => 'Segmentation', 'colour' => '#B02323'],
-    TaskTypeEnum::TRANSLATION    => ['text' => 'Translation', 'colour' => '#1D8A11'],
-    TaskTypeEnum::PROOFREADING   => ['text' => 'Revising', 'colour' => '#1064C4'],
-    TaskTypeEnum::DESEGMENTATION => ['text' => 'Desegmentation', 'colour' => '#B02060'],
-    TaskTypeEnum::QUALITY        => ['text' => 'QA??', 'colour' => '#B02323'],
-    TaskTypeEnum::APPROVAL       => ['text' => 'Approval??', 'colour' => '#B02060'],
 ];
