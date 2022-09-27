c27f3f4454702efc124ec9c4c2948e784678cb91
Alan Barrett <alanabarrett0@gmail.com>
2022-09-27 16:55:19 +0100

    WIP

--- Common/Enums/TaskTypeEnum.class.php
+++ Common/Enums/TaskTypeEnum.class.php
@@ -16,11 +16,14 @@ class TaskTypeEnum
 
     public static function init () {
         $task_type_details = \SolasMatch\UI\DAO\ProjectDao::get_task_type_details();
+error_log(print_r($task_type_details, true));
         $enum_to_UI = [];
         $task_type_to_enum = [];
         foreach ($task_type_details as $task_type_detail) {
             $enum_to_UI[$task_type_detail['type_enum']] = $task_type_detail;
             if ($task_type_detail['enabled']) $task_type_to_enum[$task_type_detail['memsource_name']] = $task_type_detail['type_enum'];
         }
+error_log(print_r($enum_to_UI, true));
+error_log(print_r($task_type_to_enum, true));
     }
 }
