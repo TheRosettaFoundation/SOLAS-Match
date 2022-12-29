14bd0a37b4bbe10822d237ffd7621872accf0a78
Alan Barrett <alanabarrett0@gmail.com>
2022-12-29 14:48:45 +0000

    Always remove prefix before comparison

--- ui/RouteHandlers/ProjectRouteHandler.class.php
+++ ui/RouteHandlers/ProjectRouteHandler.class.php
@@ -1665,8 +1665,7 @@ error_log("task_id: $task_id, memsource_task for {$part['uid']} in event JOB_STA
                 $task_id = 0;
                 $unitary = 1;
                 foreach ($project_tasks as $project_task) {
-                    $title = $project_task['title'];
-                    if (strpos($project_task['internalId'], '.')) $title = substr($project_task['title'], strlen($project_task['internalId']) + 1);
+                    $title = substr($project_task['title'], strlen($project_task['internalId']) + 1);
                     if (!$task_id) { // First time through
                         $task_id = $project_task['id'];
                         $title_0 = $title;
