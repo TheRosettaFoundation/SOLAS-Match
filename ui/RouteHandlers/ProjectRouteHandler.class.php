a95275033535907637a7ad3afa1047a7c9ca51b5
Alan Barrett <alanabarrett0@gmail.com>
2023-04-25 17:53:13 +0100

    WIP

--- ui/RouteHandlers/ProjectRouteHandler.class.php
+++ ui/RouteHandlers/ProjectRouteHandler.class.php
@@ -1951,24 +1951,23 @@ error_log("task_id: $task_id, memsource_task for {$part['uid']} in event JOB_STA
 
         if ($post = $request->getParsedBody()) {
             if ($fail_CSRF = Common\Lib\UserSession::checkCSRFKey($post, 'project_add_shell_tasks')) return $response->withStatus(302)->withHeader('Location', $fail_CSRF);
-for ($count = 0; $count < 20; $count++) {
-  if (!empty($post["task_type_$count"]) {
-    $task_type = (int)$post["task_type_$count"]
-$quantity = (int)$post["quantity_$count"];
-$target_language = $post["target_language_$count"];
-if ($task_type > 0 && validate
-valisdate quantiry >1
-check language code
+            $selections = $projectDao->generate_language_selection();
+            for ($count = 0; $count < 20; $count++) {
+                if (!empty($post["task_type_$count"]) {
+                    $task_type = (int)$post["task_type_$count"];
+                    $quantity = (int)$post["quantity_$count"];
+                    $target_language = $post["target_language_$count"];
+                    if (empty(Common\Enums\TaskTypeEnum::$enum_to_UI[$task_type]) || $quantity <= 1 || empty($selections[$target_language])) continue;
 [[[
+$language_options[$selection['language_code'] . '-' . $selection['country_code']] = $selection['selection'];
+
                     if (empty($project_lang_pairs[$targetLanguageCode])) {
                         $project_lang_pairs[$targetLanguageCode] = ['targetLanguageCode' => $targetLanguageCode, 'targetLanguageName' => $targetLanguageName];
                         foreach (Common\Enums\TaskTypeEnum::$task_type_to_enum as $to_enum) $project_lang_pairs[$targetLanguageCode][$to_enum] = 0;
                     }
-                    foreach (Common\Enums\TaskTypeEnum::$task_type_to_enum as $to_enum) if ($task['taskType'] == $to_enum) $project_lang_pairs[$targetLanguageCode][$to_enum] += $task['wordCount'];
-                }
 ]]]
-  }
-}
+                }
+            }
 
                 $sourceLocale = new Common\Protobufs\Models\Locale();
                 $project = new Common\Protobufs\Models\Project();
