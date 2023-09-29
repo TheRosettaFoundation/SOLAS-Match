9b0c60b572e1741ae36662ad77f25b7aa42f0919
Alan Barrett <alanabarrett0@gmail.com>
2023-09-29 14:39:35 +0100

    roles

--- db/schema.sql
+++ db/schema.sql
@@ -9899,7 +9899,7 @@ BEGIN
     LEFT JOIN TermsAcceptedUsers ta ON u.id=ta.user_id
     WHERE
         (ta.user_id IS NULL OR ta.accepted_level!=3) AND
-        a.role=@LINGUIST AND
+        a.roles=@LINGUIST AND
         u.`created-time`>'2021-10-25 07:00:00' AND
         u.`created-time`<(NOW() - INTERVAL 204 HOUR)
     ORDER BY u.`created-time`
