9604114408b89c35167921dd47f66a6fdd0faa7d
reineira <reine.iramurikiye@clearglobal.org>
2023-10-04 10:40:31 +0200

    test

--- ui/RouteHandlers/UserRouteHandler.class.php
+++ ui/RouteHandlers/UserRouteHandler.class.php
@@ -672,10 +672,9 @@ class UserRouteHandler
 
         //$adminDao->adjust_org_admin($user_id, $org_id,$roles,$newRole );
 
+     
         echo '<script>console.log(' . json_encode($args) . ');</script>';
-        echo '<script>console.log(' . json_encode($user) . ');</script>';
-        echo '<script>console.log(' . json_encode($roles) . ');</script>';
-        echo '<script>console.log(' . json_encode($args) . ');</script>';
+        echo '<script>console.log(' . json_encode($request) . ');</script>';
 
         
         return UserRouteHandler::render("user/invite-admin.tpl",$response);
