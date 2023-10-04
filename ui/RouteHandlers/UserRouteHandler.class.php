7755a07f088585a3bd71093dfd606da86ed84204
reineira <reine.iramurikiye@clearglobal.org>
2023-10-04 13:13:27 +0200

    test

--- ui/RouteHandlers/UserRouteHandler.class.php
+++ ui/RouteHandlers/UserRouteHandler.class.php
@@ -684,13 +684,10 @@ class UserRouteHandler
                 if ($userDao->isUserVerified($user_id)) 
                     {
                         // Not sure how roles datatype works  
+                        
                         $assign=$adminDao->adjust_org_admin($user_id, $org_id, 64 , 32);
                         $used = 1; 
-                        $adminDao->setUserRole($roles, $email, $used, $org_id, $user_id, $user_id);
-                        echo '<script>console.log(' . json_encode($assign) . ');</script>';
-                        
-                     
-                                                               
+                                                                                      
                     }
                 else 
                     {
