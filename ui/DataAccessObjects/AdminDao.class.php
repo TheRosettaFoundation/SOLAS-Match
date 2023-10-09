6d62726a0226d71de0e9e572c82778015593e193
reineira <reine.iramurikiye@clearglobal.org>
2023-10-09 16:57:31 +0200

    testing injecting value in templante

--- ui/DataAccessObjects/AdminDao.class.php
+++ ui/DataAccessObjects/AdminDao.class.php
@@ -150,12 +150,13 @@ class AdminDao extends BaseDao
 
     public function get_special_registration()
     {
-        $result = LibAPI\PDOWrapper::call('get_special_registration', LibAPI\PDOWrapper::cleanseWrapStr($_SESSION['reg_data']) . ',' . LibAPI\PDOWrapper::cleanseWrapStr(Common\Lib\Settings::get('site.reg_key')) . ",0,''");
+        $data = 'test';
+        $result = LibAPI\PDOWrapper::call('get_special_registration', LibAPI\PDOWrapper::cleanseWrapStr($test) . ',' . LibAPI\PDOWrapper::cleanseWrapStr(Common\Lib\Settings::get('site.reg_key')) . ",0,''");
       
         if (empty($result)) {
-            error_log("Bad reg_data: {$_SESSION['reg_data']}");
-            unset($_SESSION['reg_data']);
-            return "Bad reg_data: {$_SESSION['reg_data']}.";
+            error_log("Bad reg_data: { $result }");
+          
+            return "Bad reg_data: {$result}.";
         }
 
         if (empty($result)) $error = 'This link is invalid.';
