9b388c286ccf763011c4c8fbbcc6836c6e4194a9
reineira <reine.iramurikiye@clearglobal.org>
2023-10-10 09:50:38 +0200

    testing injecting value in templante

--- ui/RouteHandlers/UserRouteHandler.class.php
+++ ui/RouteHandlers/UserRouteHandler.class.php
@@ -699,9 +699,9 @@ class UserRouteHandler
                 //sendInviteEmail
                 $id=$adminDao->setUserRole(NGO_ADMIN, $email, $org_id, $user_id);                              
                 $result = $adminDao->get_special_registration_record($id['0']['0'], $org_id);
-                $url = $result['0']['url'];
+                // $url = $result['0']['url'];
                 echo '<script>console.log(' . $result . ');</script>';  
-                echo '<script>console.log(' . json_encode($url) . ');</script>';  
+                // echo '<script>console.log(' . json_encode($url) . ');</script>';  
 
             }                 
            
