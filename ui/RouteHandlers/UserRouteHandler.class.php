abdb2a3cd41be094a757c0f8c782277df4f93d9c
reineira <reine.iramurikiye@clearglobal.org>
2023-10-11 13:03:27 +0200

    test

--- ui/RouteHandlers/UserRouteHandler.class.php
+++ ui/RouteHandlers/UserRouteHandler.class.php
@@ -673,7 +673,7 @@ class UserRouteHandler
         $roles = $adminDao->get_roles(Common\Lib\UserSession::getCurrentUserID());
         $org_id = $args['org_id'];
         $org = $orgDao->getOrganisation($org_id);
-        print_r($org);
+        print_r($org['0']);
         $user_id = Common\Lib\UserSession::getCurrentUserID();
         $sent_invite = $adminDao-> get_special_registration_records($user_id);
         // $sent_reg = $adminDao-> sent_special_registration_records($email);
