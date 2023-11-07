62e1cb689174c0b224d3c555825a1930227c8819
reineira <reine.iramurikiye@clearglobal.org>
2023-11-07 11:16:28 +0200

    fixing the colors and spacing

--- Common/lib/APIHelper.class.php
+++ Common/lib/APIHelper.class.php
@@ -29,6 +29,7 @@ class APIHelper
         $headers = array()
     ) {
         $url = "$url/?";
+        var_dump($destination);
         if (!empty($query_args) && count($query_args) > 0) {
             $first = true;
             foreach ($query_args as $key => $val) {
