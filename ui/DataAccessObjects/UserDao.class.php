76f018053ac35e5f014cb7857b3beaf2fd1a246f
reineira <reine.iramurikiye@clearglobal.org>
2023-11-16 14:34:06 +0200

    debug

--- ui/DataAccessObjects/UserDao.class.php
+++ ui/DataAccessObjects/UserDao.class.php
@@ -184,11 +184,11 @@ class UserDao extends BaseDao
 
 
         $args .= LibAPI\PDOWrapper::cleanse($limit).', '.
-                LibAPI\PDOWrapper::cleanse($offset).', ';
+                LibAPI\PDOWrapper::cleanseNull($offset).', ';
         
         $args .=  LibAPI\PDOWrapper::cleanse($taskType).', ';
-        $args .=  LibAPI\PDOWrapper::cleanse($sourceLanguageCode).', ';
-        $args .=  LibAPI\PDOWrapper::cleanse($targetLanguageCode); 
+        $args .=  LibAPI\PDOWrapper::cleanseNullOrWrapStr($sourceLanguageCode).', ';
+        $args .=  LibAPI\PDOWrapper::cleanseNullOrWrapStr($targetLanguageCode); 
         $result = LibAPI\PDOWrapper::call("getUserTopTasks", $args);       
 
        
