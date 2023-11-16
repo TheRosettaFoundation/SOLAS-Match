90f79360c1fa28998de888792d077669ac7604b2
reineira <reine.iramurikiye@clearglobal.org>
2023-11-16 11:31:15 +0200

    debug

--- api/DataAccessObjects/TaskDao.class.php
+++ api/DataAccessObjects/TaskDao.class.php
@@ -253,7 +253,7 @@ class TaskDao
             $args .= "0, ";
         }
         
-        var_dump($sourceLanguageCode);
+        var_dump($offset);
 
         $args .= Lib\PDOWrapper::cleanseNullOrWrapStr($limit).', '.
                 Lib\PDOWrapper::cleanseNull($offset).', ';
