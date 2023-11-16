077abaf1606af1ee48847468033cf4d8b27ac720
reineira <reine.iramurikiye@clearglobal.org>
2023-11-16 21:37:30 +0200

    debug

--- ui/RouteHandlers/UserRouteHandler.class.php
+++ ui/RouteHandlers/UserRouteHandler.class.php
@@ -617,7 +617,7 @@ class UserRouteHandler
             if ($user_id) {
                 $strict = false;
                 $topTasks = $userDao->getUserTopTasks($user_id, $strict, $itemsPerScrollPage, $filter, $offset);
-                var_dump( gettype $topTasks);
+                var_dump(gettype($topTasks));
          
                 $topTasksCount = $userDao->getUserTopTasksCount($user_id, $strict, $filter);            
                 $topTasksC =  intval($userDao->getUserTopTasksCount($user_id, $strict, $filter));
