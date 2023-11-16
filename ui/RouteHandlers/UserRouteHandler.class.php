32c9e2a66049f822d8ed36a8060e5d6d5e0e8878
reineira <reine.iramurikiye@clearglobal.org>
2023-11-16 20:29:58 +0200

    debug

--- ui/RouteHandlers/UserRouteHandler.class.php
+++ ui/RouteHandlers/UserRouteHandler.class.php
@@ -488,12 +488,12 @@ class UserRouteHandler
       
        
       
-        $payload = json_encode($topTasks);
+        // $payload = json_encode($topTasks);
         // var_dump($payload);
 
-        $response->getBody()->write($payload);
-        $response->withHeader('Content-Type' , 'application/json');
-        $response->withStatus(200);
+        header('Content-Type: application/json');
+        echo json_encode(['results' => $payload]);
+        die;
      
 
         return ($response);
