33a8094175b814122d4dfd9098871ef47048d51b
reineira <reine.iramurikiye@clearglobal.org>
2023-12-09 15:24:06 +0200

    fixing the pagination

--- ui/js/Home3.js
+++ ui/js/Home3.js
@@ -114,8 +114,8 @@ function renderTaskDetails() {
                     m +
                     "-" +
                     d +
-                    `<div class="mx-3 d-flex align-items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#f89406" class="bi bi-clock" viewBox="0 0 20 20">
+                    `<div class="mx-3 d-flex align-items-center bg-info">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#f89406" class="bi bi-clock" viewBox="0 0 20 20">
                         <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z"/>
                         <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0"/>
                         </svg>
