25b673424377a3fc0494883719c4a02c8c3f35cf
reineira <reine.iramurikiye@clearglobal.org>
2023-12-12 09:51:36 +0200

    fixing the pagination

--- ui/RouteHandlers/TaskRouteHandler.class.php
+++ ui/RouteHandlers/TaskRouteHandler.class.php
@@ -553,7 +553,7 @@ class TaskRouteHandler
          
         $payload = json_encode($results, JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP | JSON_UNESCAPED_UNICODE);
 
-        $response->getBody()->write($payload);
+        $response->getBody()->write($results);
 
         return $response->withHeader('Content-Type', 'application/json');
 
