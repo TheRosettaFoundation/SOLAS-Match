6b5e590c73fa62d85a93f2663dd9eddd5894b777
reineira <reine.iramurikiye@clearglobal.org>
2023-12-19 16:55:17 +0200

    fixing space in the time

--- ui/js/Home3.js
+++ ui/js/Home3.js
@@ -108,7 +108,7 @@ function renderTaskDetails() {
             parameters.getTranslation("common_due_by").replace(
                 "%s",
 
-                "<div class='d-flex align-items-center '>" +
+                "<div class='d-flex align-items-center me-4 '>" +
                     `<div class="me-2">` +
                     deadline.getFullYear() +
                     "-" +
@@ -116,7 +116,7 @@ function renderTaskDetails() {
                     "-" +
                     d +
                     `</div>` +
-                    `<div class=" d-flex align-items-center wrap ms-2">
+                    `<div class=" d-flex align-items-center wrap mx-2">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#f89406" class="bi bi-clock" viewBox="0 0 16 16" class="ms-2 me-1">
                         <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z"/>
                         <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0"/>
@@ -125,7 +125,7 @@ function renderTaskDetails() {
 
                   ` +
                     " " +
-                    `<span class=""> ` +
+                    `<span class="mx-2"> ` +
                     h +
                     ":" +
                     mi +
