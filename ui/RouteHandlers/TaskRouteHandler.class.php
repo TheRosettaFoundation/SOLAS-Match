97385665cd4b1bb92a1e493bb7f271871005a838
reineira <reine.iramurikiye@clearglobal.org>
2023-12-23 21:19:29 +0200

    taskClaim page

--- ui/RouteHandlers/TaskRouteHandler.class.php
+++ ui/RouteHandlers/TaskRouteHandler.class.php
@@ -1360,7 +1360,7 @@ class TaskRouteHandler
         }
 
         $extra_scripts = file_get_contents(__DIR__."/../js/TaskView3.js");
-        $extra_scripts .= "<script type=\"text/javascript\" src=\"{$app->getRouteCollector()->getRouteParser()->urlFor("home")}ui/js/lib/iframe.js\"></script>";
+        $extra_scripts .= "<script type=\"text/javascript\" src=\"{$app->getRouteCollector()->getRouteParser()->urlFor("home")}ui/js/iframe.js\"></script>";
 
         $alsoViewedTasks = [];
         $alsoViewedTasksCount = 0;
