83e96fb01b1ada18da3a6acf9e35dd3625a6b2a3
reineira <reine.iramurikiye@clearglobal.org>
2024-01-10 10:04:24 +0200

    converting to local time fixes

--- ui/js/pagination.js
+++ ui/js/pagination.js
@@ -410,7 +410,7 @@ function displayTasks(pages) {
     <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z"/>
     <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0"/>
     </svg>  <div class="mx-2"> ${time[1]}</div>  </strong>
-    <div class="ms-2"> ${timezone}</div>
+    <div class="ms-2"> </div>
 
      </div>
 
