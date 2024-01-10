086d350dc98191112a5d64adfe1abb394635c186
reineira <reine.iramurikiye@clearglobal.org>
2024-01-10 11:34:04 +0200

    converting to local time fixes

--- ui/js/pagination.js
+++ ui/js/pagination.js
@@ -400,7 +400,7 @@ function displayTasks(pages) {
         console.log(offset);
         const timeLocal = new Date(deadlineDate.getTime() + offset * 60 * 1000);
         const hour = timeLocal.getHours() % 12 || 12;
-        const min = timeLocal.getMinutes();
+        const min = timeLocal.getMinutes().toString().padStart(2, "0");
         const sec = timeLocal.getSeconds();
 
         let timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
