fcfbe48195da3ca8ca6241039ab0c34269c353a6
reineira <reine.iramurikiye@clearglobal.org>
2024-01-24 15:29:53 +0200

    fixing hover state link

--- ui/js/pagination.js
+++ ui/js/pagination.js
@@ -22,12 +22,12 @@ for (let i = 0; i < projects.length; i++) {
     secondLink.classList.add("custom-link");
 }
 
-const navItems = document.querySelectorAll(".nav-item");
+const navItems = document.querySelectorAll(".nav-link");
 
 for (let i = 0; i < navItems.length; i++) {
     navItems[i].addEventListener("click", function (e) {
         e.preventDefault();
-        this.classList.add("active");
+        console.log("clicked");
     });
 }
 
