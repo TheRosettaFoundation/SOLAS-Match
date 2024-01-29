8aeef253c007880912d65232b9f601eea3bbc49a
reineira <reine.iramurikiye@clearglobal.org>
2024-01-29 17:22:29 +0200

    fixing adding languages pair on pagination

--- ui/js/pagination.js
+++ ui/js/pagination.js
@@ -431,7 +431,7 @@ if (isPagination) {
             title.classList.add("custom-link", "fw-bold", "fs-3");
             title.href = `/task/${item.id}/view`;
             title.target = "_blank";
-            title.textContent = item.title;
+            title.innerText = item.title;
 
             const spanTitle = document.createElement("div");
             const spanImg = document.createElement("img");
