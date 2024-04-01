4cc19077e146eb5791758d22ed6a1f7acc1ba81f
reinelic <reine.iramurikiye@clearglobal.org>
2024-04-01 13:20:05 +0200

    fix: profile page

--- ui/RouteHandlers/UserRouteHandler.class.php
+++ ui/RouteHandlers/UserRouteHandler.class.php
@@ -3156,7 +3156,7 @@ EOF;
         header('Pragma: no-cache');
         header('Cache-control: no-cache, must-revalidate, no-transform');
 
-        $logo = imagecreatefrompng('/repo/SOLAS-Match/ui/img/TWB_Community_members_badge_BG-01.png');
+        $logo = imagecreatefrompng('/repo/SOLAS-Match/ui/img/profile_badge.svg');
         $size = 60;
         $angle = 0;
         $left = 950;
