613482001521e2180f45426f894c40f74842bc48
reinelic <reine.iramurikiye@clearglobal.org>
2024-04-01 14:20:05 +0200

    fix: profile page

--- ui/RouteHandlers/UserRouteHandler.class.php
+++ ui/RouteHandlers/UserRouteHandler.class.php
@@ -438,7 +438,7 @@ class UserRouteHandler
             'user_id' => $user_id,
             'org_admin' => $org_admin,
             'page_count' => $pages,
-            'roles' => !empty($user_id) ? $adminDao->get_roles($user_id) :
+            'roles' => !empty($user_id) ? $adminDao->get_roles($user_id) : 0,
             
         ));
         return UserRouteHandler::render('index-home.tpl', $response);
