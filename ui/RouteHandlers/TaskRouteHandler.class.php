121e2e29d05aa45bdc20d58bbbd4144c3f92f9f5
reinelic <reine.iramurikiye@clearglobal.org>
2024-04-09 12:40:40 +0200

    fix: user.reset.password.tpl

--- ui/RouteHandlers/TaskRouteHandler.class.php
+++ ui/RouteHandlers/TaskRouteHandler.class.php
@@ -964,15 +964,11 @@ class TaskRouteHandler
             }
 
             if ($roles & (SITE_ADMIN | PROJECT_OFFICER)) {
-                if (isset($post['word_count']) && ctype_digit($post['word_count'])) {
+                if (isset($post['word_count_partner_weighted']) && ctype_digit($post['word_count_partner_weighted'])) {
                     $task->setWordCount($post['word_count']);
-                    $projectDao->queue_asana_project($task->getProjectId());
-                } elseif (isset($post['word_count_partner_weighted']) && ctype_digit($post['word_count_partner_weighted'])) {
                     $task->set_word_count_partner_weighted($post['word_count_partner_weighted']);
                     $projectDao->queue_asana_project($task->getProjectId());
-                } elseif (isset($post['word_count']) && $post['word_count'] != "") {
-                    $word_count_err = Lib\Localisation::getTranslation('task_alter_6');
-                } elseif (isset($post['word_count_partner_weighted']) && $post['word_count_partner_weighted'] != "") {
+                 elseif (isset($post['word_count_partner_weighted']) && $post['word_count_partner_weighted'] != "" || isset($post['word_count']) && $post['word_count'] != "" ) {
                     $word_count_err = Lib\Localisation::getTranslation('task_alter_6');
                 } else {
                     $word_count_err = Lib\Localisation::getTranslation('task_alter_7');
--- ui/templating/templates/user/user.reset-password.tpl
+++ ui/templating/templates/user/user.reset-password.tpl
@@ -91,9 +91,9 @@
         </div>
         </nav>
 
-        <main class="flex-grow-1 ">
+        <main class=" ">
 
-        <div class="row d-flex flex-column align-items-center  justify-content-center" >
+        <div class="row flex-grow-1 d-flex flex-column align-items-center  justify-content-center" >
 
         {include file="handle-flash-messages.tpl"}
   
