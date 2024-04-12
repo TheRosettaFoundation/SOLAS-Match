443ee03127aabefe9b66d3fb7305d128b2e7ecaa
reinelic <reine.iramurikiye@clearglobal.org>
2024-04-12 17:26:03 +0200

    price

--- ui/RouteHandlers/TaskRouteHandler.class.php
+++ ui/RouteHandlers/TaskRouteHandler.class.php
@@ -1418,7 +1418,7 @@ class TaskRouteHandler
         if (Common\Enums\TaskTypeEnum::$enum_to_UI[$task->getTaskType()]['divide_rate_by_60']) $total_expected_cost /= 60;
         $extra_scripts .= "<script type=\"text/javascript\"  src=\"{$app->getRouteCollector()->getRouteParser()->urlFor("home")}ui/js/pagination.js?v=jdkdk9dddkedf\" defer ></script>";
 
-        if (!empty($paid_status) && $task->getWordCount() > 1) $total_expected_price = $task->getWordCount()*$paid_status['unit_rate_pricing'];
+        if (!empty($paid_status) && $task->getWordCount() > 1) $total_expected_price = $task->get_word_count_partner_weighted()*$paid_status['unit_rate_pricing'];
         if (Common\Enums\TaskTypeEnum::$enum_to_UI[$task->getTaskType()]['divide_rate_by_60']) $total_expected_price /= 60;
 
         $template_data = array_merge($template_data, array(
