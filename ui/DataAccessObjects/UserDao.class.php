d5958bde13885410932b306aaeea1adb2e4b25a1
reinelic <reine.iramurikiye@clearglobal.org>
2024-04-30 10:43:56 +0200

    adding  task chunk feature

--- ui/DataAccessObjects/UserDao.class.php
+++ ui/DataAccessObjects/UserDao.class.php
@@ -212,7 +212,7 @@ class UserDao extends BaseDao
                  LibAPI\PDOWrapper::cleanse($task_id_6);
         $result =  LibAPI\PDOWrapper::call('getNumberOfChunks', $args);
         print_r($args);
-        
+        print_r($result);
         if(empty($result)) return [] ;
 
         $chunks=[];
