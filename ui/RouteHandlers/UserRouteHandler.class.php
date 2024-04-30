2fbb93616b3b96dafc7aa648b7b5ece4ba39aab2
reinelic <reine.iramurikiye@clearglobal.org>
2024-04-30 13:45:03 +0200

    debugging  js file

--- ui/RouteHandlers/UserRouteHandler.class.php
+++ ui/RouteHandlers/UserRouteHandler.class.php
@@ -309,11 +309,8 @@ class UserRouteHandler
             }
         }
         $chunks =  $userDao->getUserTaskChunks(...$tasksIds) ;
-    
-        // $arr_results = array('tasks'=> $topTasks , 'images'=> $taskImages, 'projects'=> $projectAndOrgs, 'chunks'=> $chunks);
-        // $results = json_encode($arr_results) ;
-        $results = json_encode(['tasks'=> $topTasks , 'images' => $taskImages, 'projects'=> $projectAndOrgs]);
-     
+ 
+        $results = json_encode(['tasks'=> $topTasks , 'images' => $taskImages, 'projects'=> $projectAndOrgs], 'chunks'=>$chunks);
         $response->getBody()->write($results);
         return $response->withHeader('Content-Type', 'application/json');
     }
