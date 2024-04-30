5276eaf8497d3de77b655026bd05a62b10e72b37
reinelic <reine.iramurikiye@clearglobal.org>
2024-04-30 13:30:48 +0200

    debugging  js file

--- ui/DataAccessObjects/UserDao.class.php
+++ ui/DataAccessObjects/UserDao.class.php
@@ -211,7 +211,7 @@ class UserDao extends BaseDao
                  LibAPI\PDOWrapper::cleanse($task_id_5) . ',' .
                  LibAPI\PDOWrapper::cleanse($task_id_6);
         $result =  LibAPI\PDOWrapper::call('getNumberOfChunks', $args);
-        print_r($args);
+
     
         if(empty($result)) return [] ;
 
