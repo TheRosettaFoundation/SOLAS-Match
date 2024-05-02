9128f785603dc072e0849a555f3f6cf8d0782138
reinelic <reine.iramurikiye@clearglobal.org>
2024-05-02 11:48:28 +0200

    design fixes

--- ui/RouteHandlers/TaskRouteHandler.class.php
+++ ui/RouteHandlers/TaskRouteHandler.class.php
@@ -1423,11 +1423,10 @@ class TaskRouteHandler
         $taskStatusTexts[3] = Lib\Localisation::getTranslation('common_in_progress');
         $taskStatusTexts[4] = Lib\Localisation::getTranslation('common_complete');
         $chunks =  $userDao->getUserTaskChunks($task_id);
-        $viewedTaskIds = array_map(function($element)
-                                {
-                                return $element["id"];
-                                },$alsoViewedTasks);
-        // // $chunksAlsoViews =  $userDao->getUserTaskChunks(...$alsoViewedTasks);
+        $viewedTaskIds = [] ;
+        foreach($alsoViewedTasks as $task){
+            $viewedTaskIds = $task["id"];
+        }
 
         print_r($viewedTaskIds);
         
