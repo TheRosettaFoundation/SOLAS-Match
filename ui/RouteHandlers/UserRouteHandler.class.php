3e9a0ae4e533461a257c9d9e932310b64fbf8814
reinelic <reine.iramurikiye@clearglobal.org>
2024-05-08 15:38:13 +0200

    ajax

--- ui/RouteHandlers/UserRouteHandler.class.php
+++ ui/RouteHandlers/UserRouteHandler.class.php
@@ -1110,7 +1110,7 @@ class UserRouteHandler
         $taskDao = new DAO\TaskDao();
        
    
-       $sesskey = $request->getParsedBody;
+       $sesskey = $request->getParsedBody();
        $test = (array) $sesskey;
 
         try {
