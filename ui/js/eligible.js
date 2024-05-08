f19a1d25d23f1e3d5012a937c6eaefdd1f4502d3
reinelic <reine.iramurikiye@clearglobal.org>
2024-05-08 16:59:35 +0200

    ajax

--- ui/js/eligible.js
+++ ui/js/eligible.js
@@ -2,9 +2,7 @@ const selectEligibles = document.querySelectorAll("form .eligible");
 
 async function setEligibility({ sc, sl, tc, tl, el, user, sesskey }) {
     let url = `/set_paid_eligible_pair/${user}/sl/${sl}/sc/${sc}/tl/${tl}/tc/${tc}/eligible/${el}/`;
-
     const key = { sesskey };
-
     try {
         const response = await fetch(url, {
             method: "POST",
@@ -25,6 +23,7 @@ selectEligibles.forEach(function (curr, index, arr) {
     let codes = {};
     curr.addEventListener("change", function (e) {
         e.preventDefault();
+
         let parent = curr.parentElement;
 
         let sc = parent.querySelector(".sc").textContent;
@@ -33,8 +32,6 @@ selectEligibles.forEach(function (curr, index, arr) {
         let tc = parent.querySelector(".tc").textContent;
         let user = parent.querySelector(".user").textContent;
         let sesskey = parent.querySelector(".sesskey").textContent;
-        console.log("sesskey", sesskey);
-
         let el = curr.value;
 
         codes = {
