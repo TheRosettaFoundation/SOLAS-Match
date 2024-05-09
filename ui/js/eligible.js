795fe90c3f85b8eab232a1f9a5dace2f25a9ceea
reinelic <reine.iramurikiye@clearglobal.org>
2024-05-09 09:01:02 +0200

    assign permission for eligibility

--- ui/js/eligible.js
+++ ui/js/eligible.js
@@ -16,32 +16,31 @@ async function setEligibility({ sc, sl, tc, tl, el, user, sesskey }) {
         console.error(error);
     }
 }
+if (selectEligibles) {
+    selectEligibles.forEach(function (curr, index, arr) {
+        let codes = {};
+        curr.addEventListener("change", function (e) {
+            e.preventDefault();
+            let parent = curr.parentElement;
+            let sc = parent.querySelector(".sc").textContent;
+            let sl = parent.querySelector(".sl").textContent;
+            let tl = parent.querySelector(".tl").textContent;
+            let tc = parent.querySelector(".tc").textContent;
+            let user = parent.querySelector(".user").textContent;
+            let sesskey = parent.querySelector(".sesskey").textContent;
+            let el = curr.value;
 
-selectEligibles.forEach(function (curr, index, arr) {
-    let codes = {};
-    curr.addEventListener("change", function (e) {
-        e.preventDefault();
+            codes = {
+                sc,
+                sl,
+                tl,
+                tc,
+                el,
+                user,
+                sesskey,
+            };
 
-        let parent = curr.parentElement;
-
-        let sc = parent.querySelector(".sc").textContent;
-        let sl = parent.querySelector(".sl").textContent;
-        let tl = parent.querySelector(".tl").textContent;
-        let tc = parent.querySelector(".tc").textContent;
-        let user = parent.querySelector(".user").textContent;
-        let sesskey = parent.querySelector(".sesskey").textContent;
-        let el = curr.value;
-
-        codes = {
-            sc,
-            sl,
-            tl,
-            tc,
-            el,
-            user,
-            sesskey,
-        };
-
-        setEligibility(codes);
+            setEligibility(codes);
+        });
     });
-});
+}
