04d3b923c901499034c75c3c4d30b63ca4c8640a
reinelic <reine.iramurikiye@clearglobal.org>
2024-05-27 22:50:19 +0200

    fix: fixing the datetimepicker

--- ui/js/demo.js
+++ ui/js/demo.js
@@ -5,14 +5,14 @@
             showClose: true,
             showClear: true,
             showTodayButton: true,
-            format: "MM/DD/YYYY hh:mm:ss",
+            format: "MM/DD/YYYY hh:mm:ss A ",
         });
         $("#id_1").datetimepicker({
             allowInputToggle: true,
             showClose: true,
             showClear: true,
             showTodayButton: true,
-            format: "MM/DD/YYYY HH:mm:ss",
+            format: "MM/DD/YYYY hh:mm:ss",
         });
         $("#id_2").datetimepicker({
             allowInputToggle: true,
