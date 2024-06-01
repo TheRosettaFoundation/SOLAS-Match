2f503a20a93187327725b5f8dbf6ea221c44dfec
reinelic <reine.iramurikiye@clearglobal.org>
2024-06-01 12:07:59 +0200

    loading days lib

--- ui/RouteHandlers/TaskRouteHandler.class.php
+++ ui/RouteHandlers/TaskRouteHandler.class.php
@@ -896,7 +896,9 @@ class TaskRouteHandler
         // <script type=\"text/javascript\" src=\"{$app->getRouteCollector()->getRouteParser()->urlFor("home")}ui/js/DeadlinePicker1.js\"></script>";
 
         $extra_scripts = "
-         <script type=\"text/javascript\" src=\"{$app->getRouteCollector()->getRouteParser()->urlFor("home")}ui/js/lib/jquery-ui-timepicker-addon.js\"></script>";
+         <script type=\"text/javascript\" src=\"{$app->getRouteCollector()->getRouteParser()->urlFor("home")}ui/js/lib/jquery-ui-timepicker-addon.js\"></script>
+         <script type=\"text/javascript\"  src=\"{$app->getRouteCollector()->getRouteParser()->urlFor("home")}ui/js/datetimepicker.js\" defer ></script>
+         ";
 
 
         $memsource_task = $projectDao->get_memsource_task($task_id);
