8a99bfb628558860922e6f54542f18db0d57a462
reinelic <reine.iramurikiye@clearglobal.org>
2024-06-01 12:55:20 +0200

    loading days lib

--- ui/js/datetimepicker.js
+++ ui/js/datetimepicker.js
@@ -1,36 +1,31 @@
 var dUTC = $("#deadline_field").val();
-const date = new Date(dUTC) ;
+const date = new Date(dUTC);
 const dayjs = window.dayjs;
-dayjs.extend(window.dayjs_plugin_utc)
+dayjs.extend(window.dayjs_plugin_utc);
 
-const test = dayjs(date).utc('z').local().format('YYYY-MM-DD HH:mm:ss') ;
+const test = dayjs(date).utc("z").local().format("YYYY-MM-DD HH:mm:ss");
 
-console.log(test);
+let domi = document.getElementById("datetimepicker1Input");
 
-let domi = document.getElementById('datetimepicker1Input');
-
-domi.setAttribute("value",test)
+domi.setAttribute("value", test);
 
 const datetimepicker1 = new tempusDominus.TempusDominus(
-document.getElementById('datetimepicker1'),
-{
-   //put your config here
-   display:{
-    components:
+    document.getElementById("datetimepicker1"),
     {
-        year:true,
-        month:true,
-        date:true,
-        hours:true,
-        minutes:true,
-        seconds:true
+        //put your config here
+        display: {
+            components: {
+                year: true,
+                month: true,
+                date: true,
+                hours: true,
+                minutes: true,
+                seconds: true,
+            },
+        },
+
+        localization: {
+            format: "yyyy-MM-dd HH:mm:ss",
+        },
     }
-   },
-
-   localization:{
-    format:'yyyy-MM-dd HH:mm:ss'
-   },
-}
 );
-
-console.log(datetimepicker1);
