8ae2e02df7aabd5e3120c38a9e3f19f3074a9b01
reinelic <reine.iramurikiye@clearglobal.org>
2024-06-03 10:54:24 +0200

    fixing utc

--- ui/js/datetimepicker.js
+++ ui/js/datetimepicker.js
@@ -4,10 +4,8 @@ console.log(dUTC);
 
 const dayjs = window.dayjs;
 dayjs.extend(window.dayjs_plugin_utc);
+dayjs.extend(window.dayjs_plugin_timezone);
 const dateUTCL = dayjs.local(dUTC);
-const dateUTC = dayjs.utc(dUTC);
-console.log(dateUTC);
-console.log(dateUTCL);
 
 const test = dayjs(dUTC).utc().local().format("YYYY-MM-DD HH:mm:ss").toString();
 
@@ -44,8 +42,8 @@ document
     .getElementById("datetimepicker1Input")
     .addEventListener("change", (e) => {
         console.log(e.target.value);
-        let local = dayjs(e.target.value);
-        let newDateUtc = local.utc();
+        let local = dayjs.local(e.target.value);
+        let newDateUtc = dayjs.tz("Coordinated Universal Time");
         utc.value = newDateUtc;
         console.log(newDateUtc);
     });
