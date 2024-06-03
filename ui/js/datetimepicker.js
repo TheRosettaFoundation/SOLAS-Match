3f84888b1bce6c3a277b419bbecb6e2d6b312a84
reinelic <reine.iramurikiye@clearglobal.org>
2024-06-03 12:45:07 +0200

    fixing utc

--- ui/js/datetimepicker.js
+++ ui/js/datetimepicker.js
@@ -1,33 +1,49 @@
+// var dUTC = $("#deadline_field").val();
+// var date = new Date(dUTC).toUTCString();
+
+// console.log(dUTC);
+
+// const dayjs = window.dayjs;
+// dayjs.extend(window.dayjs_plugin_utc);
+
+// var parsed = dayjs(dUTC);
+
+// console.log(parsed);
+// let parsedT = dayjs.utc(date);
+// let local = parsedT.local();
+
+// console.log(parsedT);
+// console.log(local);
+// const utcFrom = local.utc();
+// console.log(utcFrom);
+
+// const test = dayjs(dUTC).utc(true).local().format("YYYY-MM-DD HH:mm:ss");
+
+// console.log(test);
+
+// const testUTC = test.utc();
+
+// console.log(testUTC);
+
+// let domi = document.getElementById("datetimepicker1Input");
+
 var dUTC = $("#deadline_field").val();
-var date = new Date(dUTC).toUTCString();
+console.log("Original UTC time:", dUTC);
 
-console.log(dUTC);
-
+// Parse the UTC time using dayjs
 const dayjs = window.dayjs;
 dayjs.extend(window.dayjs_plugin_utc);
 
-var parsed = dayjs(dUTC);
+var parsedUTC = dayjs.utc(dUTC);
+console.log("Parsed UTC time:", parsedUTC.format());
 
-console.log(parsed);
-let parsedT = dayjs.utc(date);
-let local = parsedT.local();
+// Convert the parsed UTC time to local time
+let localTime = parsedUTC.local();
+console.log("Converted to local time:", localTime.format());
 
-console.log(parsedT);
-console.log(local);
-const utcFrom = local.utc();
-console.log(utcFrom);
+// let domi = document.getElementById("datetimepicker1Input");
 
-const test = dayjs(dUTC).utc(true).local().format("YYYY-MM-DD HH:mm:ss");
-
-console.log(test);
-
-const testUTC = test.utc();
-
-console.log(testUTC);
-
-let domi = document.getElementById("datetimepicker1Input");
-
-domi.setAttribute("value", test);
+domi.setAttribute("value", localTime);
 
 const datetimepicker1 = new tempusDominus.TempusDominus(
     document.getElementById("datetimepicker1"),
