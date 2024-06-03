cfa9b922beb31ff73aeafae3508463ac200154f1
reinelic <reine.iramurikiye@clearglobal.org>
2024-06-03 13:40:05 +0200

    fixing utc

--- ui/js/datetimepicker.js
+++ ui/js/datetimepicker.js
@@ -1,16 +1,13 @@
 var dUTC = $("#deadline_field").val();
-console.log("Original UTC time:", dUTC);
 
 // Parse the UTC time using dayjs
 const dayjs = window.dayjs;
 dayjs.extend(window.dayjs_plugin_utc);
 
 var parsedUTC = dayjs.utc(dUTC);
-console.log("Parsed UTC time:", parsedUTC.format());
 
 // Convert the parsed UTC time to local time
 let localTime = parsedUTC.local();
-console.log("Converted to local time:", localTime.format(""));
 
 let domi = document.getElementById("datetimepicker1Input");
 
@@ -44,11 +41,6 @@ document
     .getElementById("datetimepicker1Input")
     .addEventListener("change", (e) => {
         let local = dayjs(e.target.value);
-        // No need to call utc() on the local object itself
-        // Convert to UTC directly with dayjs.utc()
         let utcTime = dayjs.utc(local);
-        console.log(local.format());
-        console.log(utcTime.format("YYYY-MM-DD HH:mm:ss"));
         deadline.setAttribute("value", utcTime.format("YYYY-MM-DD HH:mm:ss"));
-        console.log(deadline);
     });
