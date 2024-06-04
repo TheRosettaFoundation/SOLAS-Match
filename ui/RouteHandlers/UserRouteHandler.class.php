f597b533ce7e164ffe9cba8837699792944b9c6f
reinelic <reine.iramurikiye@clearglobal.org>
2024-06-04 14:20:26 +0200

    generating pdf

--- ui/RouteHandlers/UserRouteHandler.class.php
+++ ui/RouteHandlers/UserRouteHandler.class.php
@@ -3505,11 +3505,11 @@ EOF;
     $fill = 0;
     foreach($data as $row) {
         $pdf->Cell($w[0], 6, $row[0], 'LR', 0, 'L');
-        $pdf->Cell($w[2], 6, $row[10] ,$name, 'LR', 0, 'R');
+        $pdf->Cell($w[1], 6, $row[11], 'LR', 0, 'L');
+        $pdf->Cell($w[2], 6, $row[10], 'LR', 0, 'R');
         $pdf->Cell($w[3], 6, $row[3], 'LR', 0, 'R');
         $pdf->Cell($w[2], 6, $row[2], 'LR', 0, 'R');
-        $pdf->Cell($w[3], 6, $row[7], 'LR', 0, 'R'); 
-        $pdf->Cell($w[3], 6, $row[7], 'LR', 0, 'R');           
+        $pdf->Cell($w[3], 6, $row[7], 'LR', 0, 'R');       
         $pdf->Ln();
       
     }
