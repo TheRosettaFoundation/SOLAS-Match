9b3f44ff655a9f09e0f698c49c0cb473eaf2d747
reinelic <reine.iramurikiye@clearglobal.org>
2024-06-04 15:01:54 +0200

    generating pdf

--- ui/RouteHandlers/UserRouteHandler.class.php
+++ ui/RouteHandlers/UserRouteHandler.class.php
@@ -3508,54 +3508,46 @@ $tbl = <<<EOD
   <td width="80">XXXX</td>
   <td align="center" width="45">XXXX<br />XXXX</td>
  </tr>
- <tr>
-  <td width="30" align="center" rowspan="3">2.</td>
-  <td width="30" align="center"><b></b></td>
-  <td width="140" rowspan="6">XXXX<br />XXXX<br />XXXX<br />XXXX<br />XXXX<br />XXXX<br />XXXX<br />XXXX</td>
-  <td width="140">XXXX<br />XXXX</td>
-  <td width="80">XXXX<br />XXXX</td>
-  <td width="80">XXXX</td>
-  <td align="center" width="45">XXXX<br />XXXX</td>
- </tr>
+ 
  
 
 </table>
 EOD;
 
-    $pdf->writeHTML($html, true, false, true, false, '');
-    $pdf->writeHTML($tbl, true, false, false, false, '');
-    $pdf->SetFillColor(239, 239, 240);
-    $pdf->SetTextColor(255,255,245);
-    $pdf->SetDrawColor(128, 0, 0);
-    $pdf->SetLineWidth(0.3);
-    $pdf->SetFont('', 'B');
-    // Header
-    $w = array(40, 60, 40, 45 ,40 ,45);
-    $num_headers = count($header);
-    for($i = 0; $i < $num_headers; ++$i) {
-        $pdf->Cell($w[$i], 7, $header[$i], 1, 0, 'C', 1);
-    }
-    $pdf->Ln();
-    // Color and font restoration
-    $pdf->SetTextColor(0);
-    $pdf->SetFont('');
-    // Data
-    $fill = 0;
-    foreach($data as $row) {
-        $pdf->Cell($w[0], 6, $row[0], 'LR', 0, 'L');
-        $pdf->Cell($w[1], 15, $row[11], 'LR', 0, 'L');
-        $pdf->Cell($w[2], 6, $row[10], 'LR', 0, 'R');
-        $pdf->Cell($w[3], 6, $row[3], 'LR', 0, 'R');
-        $pdf->Cell($w[2], 6, $row[2], 'LR', 0, 'R');
-        $pdf->Cell($w[3], 6, $row[7], 'LR', 0, 'R');       
-        $pdf->Ln();
+    // $pdf->writeHTML($html, true, false, true, false, '');
+    // $pdf->writeHTML($tbl, true, false, false, false, '');
+    // $pdf->SetFillColor(239, 239, 240);
+    // $pdf->SetTextColor(255,255,245);
+    // $pdf->SetDrawColor(128, 0, 0);
+    // $pdf->SetLineWidth(0.3);
+    // $pdf->SetFont('', 'B');
+    // // Header
+    // $w = array(40, 60, 40, 45 ,40 ,45);
+    // $num_headers = count($header);
+    // for($i = 0; $i < $num_headers; ++$i) {
+    //     $pdf->Cell($w[$i], 7, $header[$i], 1, 0, 'C', 1);
+    // }
+    // $pdf->Ln();
+    // // Color and font restoration
+    // $pdf->SetTextColor(0);
+    // $pdf->SetFont('');
+    // // Data
+    // $fill = 0;
+    // foreach($data as $row) {
+    //     $pdf->Cell($w[0], 6, $row[0], 'LR', 0, 'L');
+    //     $pdf->Cell($w[1], 15, $row[11], 'LR', 0, 'L');
+    //     $pdf->Cell($w[2], 6, $row[10], 'LR', 0, 'R');
+    //     $pdf->Cell($w[3], 6, $row[3], 'LR', 0, 'R');
+    //     $pdf->Cell($w[2], 6, $row[2], 'LR', 0, 'R');
+    //     $pdf->Cell($w[3], 6, $row[7], 'LR', 0, 'R');       
+    //     $pdf->Ln();
       
-    }
-    $pdf->Cell(array_sum($w), 0, '', 'T');
+    // }
+    // $pdf->Cell(array_sum($w), 0, '', 'T');
    
-    $pdf->Cell(20, 10, "Issued on " . date("d F Y"), 0, false, 'L', 0, '', 0, false, 'T', 'M');
-    $pdf->Cell(0, 9, "Ref: $valid_key", 0, false, 'R', 0, '', 0, false, 'T', 'M' );
-    $pdf->lastPage();
+    // $pdf->Cell(20, 10, "Issued on " . date("d F Y"), 0, false, 'L', 0, '', 0, false, 'T', 'M');
+    // $pdf->Cell(0, 9, "Ref: $valid_key", 0, false, 'R', 0, '', 0, false, 'T', 'M' );
+    // $pdf->lastPage();
 
     $file_name = 'Invoice_.pdf';
     $pdf->Output($file_name, 'I');
