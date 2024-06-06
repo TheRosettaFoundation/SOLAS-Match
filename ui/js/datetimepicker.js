10f9735774d9b1ad8b0e218c2171177d449b15ec
reinelic <reine.iramurikiye@clearglobal.org>
2024-06-06 10:20:23 +0200

    debug  midnight hour

--- ui/js/datetimepicker.js
+++ ui/js/datetimepicker.js
@@ -40,8 +40,8 @@ document
     .getElementById("datetimepicker1Input")
     .addEventListener("change", (e) => {
         let local = dayjs(e.target.value);
-        console.log(local);
-        let utcTime = dayjs.utc(local).toString();
-        console.log(utcTime).toString();
+        console.log(local.toString());
+        let utcTime = dayjs.utc(local);
+        console.log(utcTime.toString());
         deadline.setAttribute("value", utcTime.format("YYYY-MM-DD HH:mm:ss"));
     });
