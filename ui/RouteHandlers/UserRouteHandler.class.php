c44a3a7a476299c6f1163f73871b950cb90c019e
Alan Barrett <alanabarrett0@gmail.com>
2024-06-07 17:15:53 +0100

    no $valid_key

--- ui/RouteHandlers/UserRouteHandler.class.php
+++ ui/RouteHandlers/UserRouteHandler.class.php
@@ -3512,7 +3512,6 @@ EOD;
     $pdf->writeHTML($html, true, false, true, false, '');
     $pdf->writeHTML($tbl, true, false, false, false, '');
     $pdf->Cell(20, 10, "Issued on " . date("d F Y"), 0, false, 'L', 0, '', 0, false, 'T', 'M');
-    $pdf->Cell(0, 9, "Ref: $valid_key", 0, false, 'R', 0, '', 0, false, 'T', 'M' );
     $pdf->lastPage();
 
     $pdf->Output($invoice['filename'], 'I');
