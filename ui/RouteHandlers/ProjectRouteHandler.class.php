7b8a0f8f8a704eb7d78c54f65d98444882de9143
reinelic <reine.iramurikiye@clearglobal.org>
2024-06-10 14:58:00 +0200

    working on the richEditor

--- ui/RouteHandlers/ProjectRouteHandler.class.php
+++ ui/RouteHandlers/ProjectRouteHandler.class.php
@@ -1165,7 +1165,7 @@ error_log("task_id: $task_id, memsource_task for {$part['uid']} in event JOB_STA
 
         $projectDao = new DAO\ProjectDao();
         $taskDao    = new DAO\TaskDao();
-        print_r($post['project_description']);
+        print_r($post['project_impact']);
 
 
         if (empty($_SESSION['SESSION_CSRF_KEY'])) {
