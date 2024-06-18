33af283258a8822bb224ddd4a04eb817e964dc8b
reinelic <reine.iramurikiye@clearglobal.org>
2024-06-18 14:08:58 +0200

    fixing invoice

--- ui/RouteHandlers/UserRouteHandler.class.php
+++ ui/RouteHandlers/UserRouteHandler.class.php
@@ -3591,7 +3591,7 @@ foreach ($rows as $index => $row) {
     $tbl .= <<<EOF
     <tr>
     <td width="30" align="center"><b>$number</b></td>
-    <td width="300"  style="padding-right:10px; padding-top:10px;"> $description <br /><span style="font-weight:bold;"> $project </span> <br /> $language <br /> $type<br /></td>
+    <td width="300"  style="padding-right:10px; padding-top:10px;"> $description <br /><span style="font-weight:bold;"> $project </span> <br /> <span> $language </span> br /><span> $type</span><br /></td>
     <td width="140" align="center">$purchase_order</td>
     <td width="200" align="center">$unit</td>
     <td width="100" align="center">$unit_rate</td>
