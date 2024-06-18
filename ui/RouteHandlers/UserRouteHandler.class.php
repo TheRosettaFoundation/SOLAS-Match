58828778ae39dda0e4274f7b91013d8332f1a48e
reinelic <reine.iramurikiye@clearglobal.org>
2024-06-18 12:42:19 +0200

    fixing invoice

--- ui/RouteHandlers/UserRouteHandler.class.php
+++ ui/RouteHandlers/UserRouteHandler.class.php
@@ -3518,7 +3518,7 @@ $badge = <<<EOF
                          <div style="font-size: 14px; border: 2px solid black; width: 20px; height: 50px; display: inline-block; padding: 5px; border-radius: 5px; text-align: left;">
                                  $status_text
                             </div>
-                               <div style="font-size: 14px; border: 2px solid ' . $borderColor . ' ; width: 20px; height: 50px; display: inline-block; padding: 5px; border-radius: 5px; text-align: left;">
+                               <div style="font-size: 14px; border: 2px solid ' . $badge_style . ' ; width: 20px; height: 50px; display: inline-block; padding: 5px; border-radius: 5px; text-align: left;">
                                  $status_text
                             </div>
      
