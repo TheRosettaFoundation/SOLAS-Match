265a40f620f22e9ee4032a36b83709912d7f1c8a
reinelic <reine.iramurikiye@clearglobal.org>
2024-06-19 14:42:53 +0200

    fixing the invoice

--- ui/RouteHandlers/UserRouteHandler.class.php
+++ ui/RouteHandlers/UserRouteHandler.class.php
@@ -3487,7 +3487,7 @@ $badge = <<<EOF
                     </td>
               <td width="35%"></td>  
               <td class="header1" rowspan="2" align="left" valign="middle"
-                    width="15%">
+                    width="12%">
                          <div style="font-size: 12px; font-weight:bold ; border: 1px solid black; width: 20px; height: 50px; display: inline-block; padding: 5px; border-radius: 5px; text-align:left ;">
                                  $badge_text
                             </div>
