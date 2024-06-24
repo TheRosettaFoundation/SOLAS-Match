047cfc24a4a796916f287f9612f5936de4a6b993
reinelic <reine.iramurikiye@clearglobal.org>
2024-06-24 11:44:47 +0200

    adjusting richText

--- ui/RouteHandlers/ProjectRouteHandler.class.php
+++ ui/RouteHandlers/ProjectRouteHandler.class.php
@@ -1084,7 +1084,7 @@ error_log("task_id: $task_id, memsource_task for {$part['uid']} in event JOB_STA
             $extra_scripts .= file_get_contents(__DIR__."/../js/TaskView3.js");
             // Load Twitter JS asynch, see https://dev.twitter.com/web/javascript/loading
             $extra_scripts .= '<script>window.twttr = (function(d, s, id) { var js, fjs = d.getElementsByTagName(s)[0], t = window.twttr || {}; if (d.getElementById(id)) return t; js = d.createElement(s); js.id = id; js.src = "https://platform.twitter.com/widgets.js"; fjs.parentNode.insertBefore(js, fjs); t._e = []; t.ready = function(f) { t._e.push(f); }; return t; }(document, "script", "twitter-wjs"));</script>';
-            $extra_scripts .= "<script type=\"text/javascript\"  src=\"{$app->getRouteCollector()->getRouteParser()->urlFor("home")}ui/js/richEditor.js\" defer ></script>";
+            $extra_scripts .= "<script defer type=\"text/javascript\"  src=\"{$app->getRouteCollector()->getRouteParser()->urlFor("home")}ui/js/richEditor.js\" defer ></script>";
             $template_data = array_merge($template_data, array(
                     "org" => $org,
                     "extra_scripts" => $extra_scripts,
