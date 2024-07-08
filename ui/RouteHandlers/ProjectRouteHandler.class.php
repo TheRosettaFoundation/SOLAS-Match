d457643670c3b98aa816dd3e135b96c913ce6f29
reinelic <reine.iramurikiye@clearglobal.org>
2024-07-08 12:34:02 +0200

    manually updating strict sourcing

--- ui/RouteHandlers/ProjectRouteHandler.class.php
+++ ui/RouteHandlers/ProjectRouteHandler.class.php
@@ -1108,7 +1108,9 @@ error_log("task_id: $task_id, memsource_task for {$part['uid']} in event JOB_STA
           
             $extra_scripts  = "<script type=\"text/javascript\" src=\"{$app->getRouteCollector()->getRouteParser()->urlFor("home")}resources/bootstrap/js/bootstrap.min.js\"></script>";
             $extra_scripts .= '<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.2/jquery.validate.min.js" type="text/javascript"></script>';
+            $extra_scripts .= '<script type="text/javascript" defer>';
             $extra_scripts .= file_get_contents(__DIR__."/../js/project-view1.js");
+            $extra_scripts .= '</script>';
             $extra_scripts .= file_get_contents(__DIR__."/../js/TaskView3.js");
             // Load Twitter JS asynch, see https://dev.twitter.com/web/javascript/loading
             $extra_scripts .= '<script>window.twttr = (function(d, s, id) { var js, fjs = d.getElementsByTagName(s)[0], t = window.twttr || {}; if (d.getElementById(id)) return t; js = d.createElement(s); js.id = id; js.src = "https://platform.twitter.com/widgets.js"; fjs.parentNode.insertBefore(js, fjs); t._e = []; t.ready = function(f) { t._e.push(f); }; return t; }(document, "script", "twitter-wjs"));</script>';
