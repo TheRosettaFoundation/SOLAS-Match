36ac97deab0c5e36b7632252d1af07d148bbed38
reinelic <reine.iramurikiye@clearglobal.org>
2024-07-08 16:48:43 +0200

    manually updating strict sourcing

--- ui/DataAccessObjects/TaskDao.class.php
+++ ui/DataAccessObjects/TaskDao.class.php
@@ -327,14 +327,14 @@ error_log("createTaskDirectly: $args");
     public function update_native_matching_phase_1()
     {
         $ret = 0;
-        $result = LibAPI\PDOWrapper::call('update_native_matching_phase_1',LibAPI\PDOWrapper::cleanse(null));
+        $result = LibAPI\PDOWrapper::call('update_native_matching_phase_1',LibAPI\PDOWrapper::cleanseNull(null));
         return $ret;
     }
 
     public function update_native_matching_phase_2()
     {
         $ret = 0;
-        $result = LibAPI\PDOWrapper::call('update_native_matching_phase_2',LibAPI\PDOWrapper::cleanse(null));
+        $result = LibAPI\PDOWrapper::call('update_native_matching_phase_2',LibAPI\PDOWrapper::cleanseNull(null));
         return $ret;
     }
 
