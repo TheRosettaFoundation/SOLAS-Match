e1e67103f547228e87cd4cc69b817d6538e5e1db
Alan Barrett <alanabarrett0@gmail.com>
2024-07-13 18:15:33 +0100

    tq.native_matching

--- db/schema.sql
+++ db/schema.sql
@@ -11122,6 +11122,7 @@ BEGIN
         ttd.type_enum,
         ttd.type_text,
         ttd.pricing_and_recognition_unit_text_hours,
+        tq.native_matching,
         IF(t.`word-count`>1, IF(ttd.divide_rate_by_60, t.`word-count`/60, t.`word-count`), 0) AS total_words,
         IF(tp.payment_status IS NOT NULL, tp.payment_status, 0) AS payment_status,
         IF(tp.payment_status IS NOT NULL                           , IF(t.`word-count`>1, IF(ttd.divide_rate_by_60, t.`word-count`*tp.unit_rate/60, t.`word-count`*tp.unit_rate), 0), 0) AS total_expected_cost,
@@ -11141,6 +11142,7 @@ BEGIN
         tp.status_changed
     FROM      Tasks      t
     JOIN      task_type_details ttd ON t.`task-type_id`=ttd.type_enum
+    JOIN      RequiredTaskQualificationLevels tq ON t.id=tq.task_id
     LEFT JOIN TaskPaids tp ON t.id=tp.task_id
     LEFT JOIN TaskClaims tc ON t.id=tc.task_id
     WHERE t.project_id=pID;
