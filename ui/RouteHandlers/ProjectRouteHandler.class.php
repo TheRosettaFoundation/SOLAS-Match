6f176fb82a1810df2869b62c4fef350d4beddc45
reinelic <reine.iramurikiye@clearglobal.org>
2024-07-17 12:30:10 +0200

    selecting count

--- ui/RouteHandlers/ProjectRouteHandler.class.php
+++ ui/RouteHandlers/ProjectRouteHandler.class.php
@@ -1132,6 +1132,7 @@ error_log("task_id: $task_id, memsource_task for {$part['uid']} in event JOB_STA
 
           
             $extra_scripts  = "<script type=\"text/javascript\" src=\"{$app->getRouteCollector()->getRouteParser()->urlFor("home")}resources/bootstrap/js/bootstrap.min.js\"></script>";
+            $extra_scripts .= "<script defer type=\"text/javascript\" src=\"{$app->getRouteCollector()->getRouteParser()->urlFor("home")}ui/js/taskRestrictions.js\"></script>";
             $extra_scripts .= '<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.2/jquery.validate.min.js" type="text/javascript"></script>';
             $extra_scripts .= file_get_contents(__DIR__."/../js/project-view1.js");
             $extra_scripts .= file_get_contents(__DIR__."/../js/TaskView3.js");   
