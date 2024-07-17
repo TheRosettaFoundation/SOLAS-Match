61ad82178ae5a833f4972291732190ed7f41141b
reinelic <reine.iramurikiye@clearglobal.org>
2024-07-17 15:45:02 +0200

    updating

--- ui/js/taskRestrictions.js
+++ ui/js/taskRestrictions.js
@@ -126,8 +126,7 @@ restrictionsB.forEach((elt) => {
         uniqueElements.forEach((elt) => {
             let extendedEL = `<div class="d-flex mt-4 mb-2 align-items-center justify-content-between extended">
                 <div class="me-4 elt"></div>
-                <span>Matching CMs : #</span>
-                <span>Successful CMs : #</span>
+               
                 <select class="form-select ms-2 w-50" aria-label="Default select example">
                 <option selected> Select Restrictions</option> 
                     <option value="0"> No restriction <span>Matching CMs : #</span>
