de5e06fbfc2271f013ea849b9c6099faa9e10ab1
reinelic <reine.iramurikiye@clearglobal.org>
2024-07-17 14:30:05 +0200

    selecting count

--- ui/RouteHandlers/ProjectRouteHandler.class.php
+++ ui/RouteHandlers/ProjectRouteHandler.class.php
@@ -721,6 +721,16 @@ error_log("task_id: $task_id, memsource_task for {$part['uid']} in event JOB_STA
         $sesskey = Common\Lib\UserSession::getCSRFKey();
         print_r($post["translators_count"]);
         
+        if($post['translators_count']){
+
+
+            $users_count_claim = $taskDao->count_users_who_can_claim($post['translators_count']);
+            $payload = json_encode($users_count_claim);
+            $response->getBody()->write($payload);
+            return $response ->withHeader('Content-Type','application/json') ;
+
+        }
+        
       
 
         $project = $projectDao->getProject($project_id);
