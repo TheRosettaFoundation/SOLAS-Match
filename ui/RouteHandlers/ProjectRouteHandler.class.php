e886f47d9858fae165b2898facc10258a77e263e
reinelic <reine.iramurikiye@clearglobal.org>
2024-07-17 16:49:50 +0200

    updating

--- ui/RouteHandlers/ProjectRouteHandler.class.php
+++ ui/RouteHandlers/ProjectRouteHandler.class.php
@@ -724,7 +724,7 @@ error_log("task_id: $task_id, memsource_task for {$part['uid']} in event JOB_STA
         if(isset($post['task_id'])){
 
             $users_count_claim = $taskDao->count_users_who_can_claim($post['translators_count']);
-            $payload = json_encode('result'=>[$users_count_claim]);
+            $payload = json_encode(['result'=> $users_count_claim]);
             $response->getBody()->write($payload);
             return $response ->withHeader('Content-Type','application/json') ;
 
