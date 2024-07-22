6a93fd06c309d5afd2e5afa12b8585719f2d35a9
reinelic <reine.iramurikiye@clearglobal.org>
2024-07-22 15:43:45 +0200

    fixing bugs

--- ui/js/taskRestrictions.js
+++ ui/js/taskRestrictions.js
@@ -190,12 +190,12 @@ restrictionsB.forEach((elt) => {
                 <div class="me-4 elt text-break textwrap"></div>
         
                 <select class="form-select ms-2 w-75 selectedId" aria-label="Default select example" disabled">
-                <option selected value="no"> Select Restrictions</option> 
-                    <option value="0"> </br>No restriction  <span class="nocm">, Matching CMs : 0</span> </br>
+                <option selected value="no" disabled> Select Restrictions</option> 
+                    <option value="0" disabled> </br>No restriction  <span class="nocm">, Matching CMs : 0</span> </br>
                 <span class="nosm">, Successful CMs : 0 </span> </option>
-                    <option value="1">Matching Native Language <span class="mlCM"> , Matching CMs : 0</span>
+                    <option value="1" disabled>Matching Native Language <span class="mlCM"> , Matching CMs : 0</span>
                 <span class="slCM">, Successful CMs : 0</span></option>
-                    <option value="2">Matching Native Language and Locale/Country <span class="mCM">, Matching CMs : 0 </span>
+                    <option value="2" disabled>Matching Native Language and Locale/Country <span class="mCM">, Matching CMs : 0 </span>
                 <span class="sCM">, Successful CMs : 0</span></option>
                 </select>
 
