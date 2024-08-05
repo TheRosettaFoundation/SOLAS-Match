adfffce1f8095887688b7ff879ef1598043222d6
reinelic <reine.iramurikiye@clearglobal.org>
2024-08-05 13:23:26 +0200

    replacing new lines with break

--- ui/js/projectDescClean.js
+++ ui/js/projectDescClean.js
@@ -1,17 +1,12 @@
-// Function to replace newline characters with <br> tags
 function replaceNewlinesWithBr() {
-    // Select the element with id 'display'
     const displayElement = document.getElementById("displayF");
 
-    // Check if the element exists
     if (displayElement) {
-        // Get the current HTML content of the element
         let content = displayElement.innerHTML;
 
         // Replace \n, \r, \t, \n\r with <br> tags
         content = content.replace(/(\r\n|\n|\r|\t)/g, "<br/>");
 
-        // Update the element's HTML with the modified content
         displayElement.innerHTML = content;
     } else {
         console.error('Element with id "display" not found');
