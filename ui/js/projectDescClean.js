d8b9081babdc83d16381ff90c6087400335498c4
reinelic <reine.iramurikiye@clearglobal.org>
2024-08-05 15:28:55 +0200

    replacing new lines with break

--- ui/js/projectDescClean.js
+++ ui/js/projectDescClean.js
@@ -1,3 +1,12 @@
+const url = window.location.href;
+const regex = /project\/(\d+)\//;
+const project_id = url.match(regex)[1];
+
+const sesskey = document.querySelector('input[name="sesskey"]').value;
+
+console.log(project_id);
+console.log(sesskey);
+
 function replaceNewlinesWithBr() {
     const displayElement = document.querySelector(".displayF");
 
@@ -5,12 +14,42 @@ function replaceNewlinesWithBr() {
         let content = displayElement.innerHTML;
 
         // Replace \n, \r, \t, \n\r with <br> tags
-        content = content.replace(/(\r\n|\n|\r|\t)/g, "<br/>");
+        content = content.replace(/(\\r\\n|\\n|\\r|\\t)/g, "<br/>");
+        content = content.replace(/(\\t)/g, "&nbsp;&nbsp;&nbsp;&nbsp;");
 
         displayElement.innerHTML = content;
     } else {
         console.error('Element with id "displayF" not found');
     }
+}
+
+async function cleanDescription(taskIds, matching) {
+    let url = `/project/${project_id}/alter`;
+
+    const promises = taskIds.map(async (id) => {
+        let reqBody = {
+            sesskey,
+            task_id: id,
+            matching: matching,
+        };
+        try {
+            const response = await fetch(url, {
+                method: "POST",
+                body: new URLSearchParams(reqBody),
+            });
+
+            if (!response.ok) {
+                throw new Error("error");
+            }
+
+            return response.json();
+        } catch (error) {
+            console.error(error);
+        }
+    });
+
+    const results = await Promise.all(promises);
+    return results;
 }
 
 replaceNewlinesWithBr();
