cd82773f7f6bf88db11036a6284471e281a319d5
reinelic <reine.iramurikiye@clearglobal.org>
2024-08-06 17:23:09 +0200

    adding the color code

--- ui/js/projectDescClean.js
+++ ui/js/projectDescClean.js
@@ -1,9 +1,16 @@
+function removeRGBString(rgbString) {
+    return rgbString.replace("rgb(", "").replace(")", "");
+}
+
 function cleanColorDescription() {
     let descriptionField = document.querySelector(".displayF");
 
     let spansWithStyle = descriptionField.querySelectorAll("span[style]");
 
     spansWithStyle.forEach(function (span) {
+        let test = "rgb(0 ,0 ,0)";
+        let testconsole = removeRGBString(test);
+        console.log(testconsole);
         if (span.style.color == "black" || span.style.color == "rgb(0, 0, 0)") {
             span.removeAttribute("style");
         }
