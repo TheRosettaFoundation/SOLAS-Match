df98bd13854b16f2467bac940ad841a500be1ccc
reinelic <reine.iramurikiye@clearglobal.org>
2024-08-06 17:32:07 +0200

    adding the color code

--- ui/js/projectDescClean.js
+++ ui/js/projectDescClean.js
@@ -3,10 +3,6 @@ function cleanColorDescription() {
 
     let spansWithStyle = descriptionField.querySelectorAll("span[style]");
 
-    let test = "rgb(0 ,0 ,0)";
-    let testconsole = test.substring(1, test.length - 1);
-    console.log(testconsole);
-
     spansWithStyle.forEach(function (span) {
         if (span.style.color == "black" || span.style.color == "rgb(0, 0, 0)") {
             span.removeAttribute("style");
