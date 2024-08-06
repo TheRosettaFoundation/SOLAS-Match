f711d35d222a139f32c1f1b7df279af6af1ef0b4
reinelic <reine.iramurikiye@clearglobal.org>
2024-08-06 17:29:03 +0200

    adding the color code

--- ui/js/projectDescClean.js
+++ ui/js/projectDescClean.js
@@ -1,14 +1,10 @@
-function removeRGBString(rgbString) {
-    return rgbString.replace("rgb(", "").replace(")", "");
-}
-
 function cleanColorDescription() {
     let descriptionField = document.querySelector(".displayF");
 
     let spansWithStyle = descriptionField.querySelectorAll("span[style]");
 
     let test = "rgb(0 ,0 ,0)";
-    let testconsole = removeRGBString(test);
+    let testconsole = test.substring(1, test.length - 1);
     console.log(testconsole);
 
     spansWithStyle.forEach(function (span) {
