b2cdea019bce52cb2be947cc709ce8051e2ff9fb
reinelic <reine.iramurikiye@clearglobal.org>
2024-08-07 22:39:31 +0200

    metadata feature

--- ui/RouteHandlers/ProjectRouteHandler.class.php
+++ ui/RouteHandlers/ProjectRouteHandler.class.php
@@ -2343,7 +2343,7 @@ error_log("task_id: $task_id, memsource_task for {$part['uid']} in event JOB_STA
         error_log("topicFromDB : $topicIdFromDB");
        
         $data = array(
-            'notification_level' => 3
+            'notification_level' => "3"
         );
         
         $options = array(
