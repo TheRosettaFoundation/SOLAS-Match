e8f4b3cf274ccb57b2971d5c434a30f02d45be4d
reinelic <reine.iramurikiye@clearglobal.org>
2024-08-07 11:53:15 +0200

    working on asana api

--- ui/RouteHandlers/ProjectRouteHandler.class.php
+++ ui/RouteHandlers/ProjectRouteHandler.class.php
@@ -2358,6 +2358,7 @@ error_log("task_id: $task_id, memsource_task for {$part['uid']} in event JOB_STA
         } else {
             // Decode the JSON response
             $responseData = json_decode($response, true);
+            print_r($responseData) ;
             }
 
             // Close the cURL session
