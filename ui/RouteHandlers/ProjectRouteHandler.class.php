cda393ea01281b84a5d8916e4f2c15521c797df3
reinelic <reine.iramurikiye@clearglobal.org>
2024-08-20 21:32:26 +0200

    fetching subtask

--- ui/RouteHandlers/ProjectRouteHandler.class.php
+++ ui/RouteHandlers/ProjectRouteHandler.class.php
@@ -2542,8 +2542,10 @@ error_log("task_id: $task_id, memsource_task for {$part['uid']} in event JOB_STA
                     $taskData = json_decode($taskStatusResponse, true);
                     error_log("below status is complete or ");
                     error_log($taskData['data']['completed']); 
+                    print_r($taskData['data']['completed']);
+                    break;
 
-                    curl_close($taskch);
+                 
 
 
                     
