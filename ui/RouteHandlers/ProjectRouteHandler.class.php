717f5d1c2b73da3e5d5c0e95f34e444835a17812
reinelic <reine.iramurikiye@clearglobal.org>
2024-08-20 20:08:11 +0200

    fetching subtask

--- ui/RouteHandlers/ProjectRouteHandler.class.php
+++ ui/RouteHandlers/ProjectRouteHandler.class.php
@@ -2517,8 +2517,6 @@ error_log("task_id: $task_id, memsource_task for {$part['uid']} in event JOB_STA
 
         $responseData = json_decode($response, true); 
 
-        print_r($response_data);
-
        
         $userGid = null; 
 
