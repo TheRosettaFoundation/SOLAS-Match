fc3f524f3f32a7ee704e13fb3bfb05bb8ddd1fc1
reinelic <reine.iramurikiye@clearglobal.org>
2024-08-20 14:15:53 +0200

    fetching subtask

--- ui/RouteHandlers/ProjectRouteHandler.class.php
+++ ui/RouteHandlers/ProjectRouteHandler.class.php
@@ -2541,7 +2541,7 @@ error_log("task_id: $task_id, memsource_task for {$part['uid']} in event JOB_STA
                    
                     error_log("Asana Task ID : $asanaTask");
 
-                    $data = json_encode(['data' => [ 'assignee' => $userGid ]]);
+                    $data =['data' => [ 'assignee' => $userGid ]];
                      
                     $ch = curl_init(); 
         
