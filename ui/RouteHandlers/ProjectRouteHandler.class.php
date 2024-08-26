2843f3a27efef8a24e35518ab6ea77eefc23b578
reinelic <reine.iramurikiye@clearglobal.org>
2024-08-26 12:46:22 +0200

    added contributor custom field

--- ui/RouteHandlers/ProjectRouteHandler.class.php
+++ ui/RouteHandlers/ProjectRouteHandler.class.php
@@ -2579,7 +2579,7 @@ error_log("task_id: $task_id, memsource_task for {$part['uid']} in event JOB_STA
 
                     $responseDataSub =  executeCurl($taskSubtask,'GET', null , $token) ;
 
-                    $followResponse = executeCurl($contributorFollowerUrl,'POST', followers , $token) ;
+                    $followResponse = executeCurl($contributorFollowerUrl,'POST', $followers , $token) ;
 
 
                     if(isset($responseDataSub['data']))
