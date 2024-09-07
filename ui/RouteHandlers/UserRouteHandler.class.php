5513cdfc16fd01b649aaa793e56f5bad74fb269c
Alan Barrett <alanabarrett0@gmail.com>
2024-09-07 15:57:40 +0100

    Draft Message

--- ui/RouteHandlers/UserRouteHandler.class.php
+++ ui/RouteHandlers/UserRouteHandler.class.php
@@ -3273,6 +3273,7 @@ EOF;
 
         $status = $invoice['status'];
         $paid = $status&2 ? 'PAID' : '';
+        $draft_message = $status == 1 ? 'Please review invoice and email it to linguistspayments@clearglobal.org' : '';
         if ($status&4) $paid = 'BOUNCED';
 
         $email = $invoice['email'];
@@ -3310,7 +3311,7 @@ EOF;
 $html = <<<EOF
         <table width="100%" cellspacing="0" cellpadding="55%">
         <tr valign="bottom">
-            <td class="header1" rowspan="2" align="left" valign="middle" width="33%"></td>
+            <td class="header1" rowspan="2" align="left" valign="middle" width="33%">$draft_message</td>
             <td width="35%"></td>
             <td class="header1" rowspan="2" align="left" valign="middle" width="35%">
                 <div style="font-weight:bold; float:left ; font-size:26px; text-transform:uppercase">INVOICE</div>
