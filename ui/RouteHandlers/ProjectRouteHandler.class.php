9380dfa783e26c5c89960f269ed43241514d80f2
reinelic <reine.iramurikiye@clearglobal.org>
2024-09-09 12:41:53 +0200

    distributing total word count

--- ui/RouteHandlers/ProjectRouteHandler.class.php
+++ ui/RouteHandlers/ProjectRouteHandler.class.php
@@ -1099,7 +1099,7 @@ error_log("task_id: $task_id, memsource_task for {$part['uid']} in event JOB_STA
 
                     print_r($totalDistributed) ;
 
-                    print_r($distribution) ;
+    
                     
                     $taskDistribution = [] ;
 
