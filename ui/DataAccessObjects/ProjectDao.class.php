68dbefcfa7dde20343176452079881eff87620b9
reinelic <reine.iramurikiye@clearglobal.org>
2024-09-11 16:28:51 +0200

    merge richText edit

--- ui/DataAccessObjects/ProjectDao.class.php
+++ ui/DataAccessObjects/ProjectDao.class.php
@@ -74,29 +74,20 @@ class ProjectDao extends BaseDao
     public function get_linguist_project_tasksummary($projectId)
     {
         $results = LibAPI\PDOWrapper::call('get_linguist_project_tasksummary', LibAPI\PDOWrapper::cleanse($projectId));
-        $linguist_summary = [] ;
-      
+        $linguist_summary = [] ;    
             foreach ($results as $result) {
-
                 $linguist = $result['linguistDisplayName'];
                 $taskType = $result['taskTypeStatus'];
                 $count = $result['num'];
-
                 if(!isset($linguist_summary[$linguist])){
-                    $linguist_summary[$linguist] = [] ;
+                    $linguist_summary[$linguist] = [];
                 }
-
                 if(!isset($linguist_summary[$linguist][$taskType])){
                     $linguist_summary[$linguist][$taskType] = [];
                 }
-
                 $linguist_summary[$linguist][$taskType] = $count ;
-                
-
-            }
-        
+            }    
         return $linguist_summary ;
-   
     }
 
     public function createProjectDirectly($project)
