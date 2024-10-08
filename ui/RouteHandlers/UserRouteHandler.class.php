ecc29abacc4246e915cfd394a4bebe4a1577e79c
Alan Barrett <alanabarrett0@gmail.com>
2024-10-08 20:29:30 +0100

    $post['linguist_t_code']

--- ui/RouteHandlers/UserRouteHandler.class.php
+++ ui/RouteHandlers/UserRouteHandler.class.php
@@ -2480,7 +2480,7 @@ error_log("result: $result");//(**)
                 elseif (empty($post['linguist_t_code'])) UserRouteHandler::flashNow('error', 'You must enter a Sun Linguist T-Code');
                 elseif (substr($post['google_drive_link'], 0, 25) != 'https://drive.google.com/') UserRouteHandler::flashNow('error', 'You must enter a valid Google Drive Folder Link');
                 else {
-                    $taskDao->insert_update_linguist_payment_information($user_id, $loggedInUserId, $post['country_id'], $post['google_drive_link'], $post['linguist_name']);
+                    $taskDao->insert_update_linguist_payment_information($user_id, $loggedInUserId, $post['country_id'], $post['google_drive_link'], $post['linguist_name'], $post['linguist_t_code']);
                     UserRouteHandler::flashNow('success', 'Success');
                 }
             }
