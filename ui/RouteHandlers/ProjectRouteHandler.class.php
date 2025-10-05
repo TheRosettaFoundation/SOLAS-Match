bd644ed610ba4d0e68a61a3a7530ea8a67532bcc
Alan Barrett <alanabarrett0@gmail.com>
2025-10-05 14:43:11 +0100

    isset

--- ui/RouteHandlers/ProjectRouteHandler.class.php
+++ ui/RouteHandlers/ProjectRouteHandler.class.php
@@ -1053,7 +1053,7 @@ error_log("task_id: $task_id, memsource_task for {$part['uid']} in event JOB_STA
                 }
             }
 
-            if ($roles & (SITE_ADMIN | PROJECT_OFFICER) && !empty($post['request_quality_checks'])) {
+            if ($roles & (SITE_ADMIN | PROJECT_OFFICER) && isset($post['request_quality_checks'])) {
                 $number = $userDao->set_quality_checks($post['request_quality_checks']);
                 UserRouteHandler::flashNow('success', "$number new languages in files now have quality checks requested, these will take a few minutes to appear.");
             }
