5a1dc4f82c2a81cf0ce48e2067166dc8961566e4
Alan Barrett <alanabarrett0@gmail.com>
2025-10-09 14:41:14 +0100

    same change

--- ui/RouteHandlers/ProjectRouteHandler.class.php
+++ ui/RouteHandlers/ProjectRouteHandler.class.php
@@ -476,12 +476,12 @@ error_log("set_memsource_task($task_id... {$part['uid']}...), success: $success"
             $top_level = $projectDao->get_top_level($part['internalId']);
             $project_tasks = $projectDao->get_tasks_for_project($project_id);
             foreach ($project_tasks as $project_task) {
-                if ($top_level == $projectDao->get_top_level($project_task['internalId']) && $project_task['task-type_id'] != Common\Enums\TaskTypeEnum::SPOT_QUALITY_INSPECTION && $project_task['task-type_id'] != Common\Enums\TaskTypeEnum::QUALITY_EVALUATION) {
+                if ($top_level == $projectDao->get_top_level($project_task['internalId'])) {
                     if ($forward_order[$taskType]) {
                          if ($forward_order[$taskType] == $project_task['task-type_id'])
                              $projectDao->set_taskclaims_required_to_make_claimable($task_id, $project_task['task_id'], $project_id);
                     }
-                    if ($reverse_order[$taskType]) {
+                    if ($reverse_order[$taskType] && $taskType != Common\Enums\TaskTypeEnum::SPOT_QUALITY_INSPECTION && $taskType != Common\Enums\TaskTypeEnum::QUALITY_EVALUATION) {
                          if ($reverse_order[$taskType] == $project_task['task-type_id'])
                              $projectDao->set_taskclaims_required_to_make_claimable($project_task['task_id'], $task_id, $project_id);
                     }
