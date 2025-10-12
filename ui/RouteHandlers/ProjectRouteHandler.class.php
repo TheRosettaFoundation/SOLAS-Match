b98a12e43cb9138eb3d1da29fb715fc804321701
Alan Barrett <alanabarrett0@gmail.com>
2025-10-12 15:49:09 +0100

    Take out test code

--- ui/RouteHandlers/ProjectRouteHandler.class.php
+++ ui/RouteHandlers/ProjectRouteHandler.class.php
@@ -2574,7 +2574,7 @@ error_log("fields: $fields targetlanguages: $targetlanguages");//(**)
             foreach ($queue_asana_projects as $queue_asana_project) {
                 if (++$count > 4) break; // Limit number done at one time, just in case
                 $projectId = $queue_asana_project['project_id'];
-                if (false && $projectId < 28433) { // Before cutover
+                if ($projectId < 28433) { // Before cutover
                     $projectDao->dequeue_asana_project($projectId);
                     break;
                 }
