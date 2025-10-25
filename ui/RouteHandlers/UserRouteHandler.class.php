eccf98429283efb962f81486e245fcbb9491b4eb
Alan Barrett <alanabarrett0@gmail.com>
2025-10-25 12:19:07 +0100

    reset

--- ui/RouteHandlers/UserRouteHandler.class.php
+++ ui/RouteHandlers/UserRouteHandler.class.php
@@ -3522,8 +3522,7 @@ foreach ($rows as $index => $row) {
                 if (!empty($_FILES['image']['size'][$i]))       $size += $_FILES['image']['size'][$i];
                 if (!empty($_FILES['attachments']['size'][$i])) $size += $_FILES['attachments']['size'][$i];
             }
-//(**)            if ($size > 10000000) {
-            if ($size > 400000) {//(**)REMOVE
+            if ($size > 10000000) {
                 UserRouteHandler::flash('error', 'Total file size must be less than 10MB.');
                 if ($org_id) return $response->withStatus(302)->withHeader('Location', $app->getRouteCollector()->getRouteParser()->urlFor('content_items_org', ['org_id' => $org_id]));
                 else         return $response->withStatus(302)->withHeader('Location', $app->getRouteCollector()->getRouteParser()->urlFor('content_items'));
