07dce049653c45f06a233dd3c1d252c9e399b66e
Alan Barrett <alanabarrett0@gmail.com>
2025-11-02 11:08:09 +0000

    2

--- ui/DataAccessObjects/ProjectDao.class.php
+++ ui/DataAccessObjects/ProjectDao.class.php
@@ -1759,7 +1759,7 @@ GROUP BY c.id, u.id';
                                 error_log("Moodle INSERT CREATE AND CLAIM courseid: $courseid, userid: " . $row['userid']);
                             } else $count_updated++;
 //(**)DISABLE ... from HERE
-if ($courseid == 18) {//(**)TEMP LIMIT FOR courseid
+if ($courseid == 2) {//(**)TEMP LIMIT FOR courseid
                             if (!empty($projects[$courseid]) && !$insert) {
                                 $project_id = $projects[$courseid];
                                 $task = new Common\Protobufs\Models\Task();
