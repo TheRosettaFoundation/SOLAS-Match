15fbf9a7c9a0e01928249fcb4a1631246b24fd3c
Alan Barrett <alanabarrett0@gmail.com>
2026-01-19 16:28:51 +0000

    tc.user_id IS NULL

--- db/schema.sql
+++ db/schema.sql
@@ -10916,9 +10916,11 @@ BEGIN
     FROM      Users               u
     JOIN      Admins              a ON u.id=a.user_id
     LEFT JOIN TermsAcceptedUsers ta ON u.id=ta.user_id
+    LEFT JOIN TaskClaims         tc ON u.id=tc.user_id
     WHERE
         (ta.user_id IS NULL OR ta.accepted_level!=3) AND
         (a.roles=@LINGUIST OR a.roles=0) AND
+        tc.user_id IS NULL AND
         u.`created-time`>'2021-10-25 07:00:00' AND
         u.`created-time`<(NOW() - INTERVAL 204 HOUR)
     ORDER BY u.`created-time`
