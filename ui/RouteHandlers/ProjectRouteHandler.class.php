940b5c673f38cea4f9bbb1338dbebdae9a397d3b
Alan Barrett <alanabarrett0@gmail.com>
2026-04-27 14:24:46 +0100

    Disable

--- ui/RouteHandlers/ProjectRouteHandler.class.php
+++ ui/RouteHandlers/ProjectRouteHandler.class.php
@@ -1517,8 +1517,8 @@ error_log("task_id: $task_id, memsource_task for {$part['uid']} in event JOB_STA
 
         $allowed = 1;
         if (!($adminDao->get_roles($user_id, $org_id)&(SITE_ADMIN | PROJECT_OFFICER | COMMUNITY_OFFICER)) && !$projectDao->get_entitlement_remaining($org_id, 0)) {
-            $allowed = 0;
-            UserRouteHandler::flashNow('error', 'This organization does not have an active package or has used all available quota for new requests. We’d be happy to help you continue! Please get in touch at <a href="mailto:ngos@clearglobal.org?subject=Packages" target="_blank">ngos@clearglobal.org</a> to discuss your options.');
+//(**)            $allowed = 0;
+//(**)            UserRouteHandler::flashNow('error', 'This organization does not have an active package or has used all available quota for new requests. We’d be happy to help you continue! Please get in touch at <a href="mailto:ngos@clearglobal.org?subject=Packages" target="_blank">ngos@clearglobal.org</a> to discuss your options.');
         }
 
         if (empty($_SESSION['SESSION_CSRF_KEY'])) {
