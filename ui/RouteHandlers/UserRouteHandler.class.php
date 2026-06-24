deae1b9d6b9b3c663672e192d9d0ce427d807b5c
Alan Barrett <alanabarrett0@gmail.com>
2026-06-24 10:45:33 +0100

    case

--- ui/RouteHandlers/UserRouteHandler.class.php
+++ ui/RouteHandlers/UserRouteHandler.class.php
@@ -1263,7 +1263,7 @@ error_log('login PUT JSON:' . print_r($data, 1));//(**)
 error_log("from Tarjimly: $first_name, $last_name, from TWB: {$userinfo->firstName}, {$userinfo->lastName}");//(**)
                 if (!empty($first_name)) $userinfo->setFirstName($first_name);
                 if (!empty($last_name))  $userinfo->setLastName($last_name);
-                $userDao->saveUserPersonalInformation($userInfo);
+                $userDao->saveUserPersonalInformation($userinfo);
 //            }
         }
         return $user;
