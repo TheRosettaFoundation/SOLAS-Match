c9c88b4f8b20a7aa2cc1b06ce7fa747662b61715
Alan Barrett <alanabarrett0@gmail.com>
2026-07-08 17:19:16 +0100

    after "+"

--- ui/RouteHandlers/UserRouteHandler.class.php
+++ ui/RouteHandlers/UserRouteHandler.class.php
@@ -1865,8 +1865,8 @@ error_log('login PUT JSON:' . print_r($data, 1));//(**)
             var fieldWrapper = $("<div class=\"row g-2 align-items-center mb-2\" id=\"field" + intId + "\"/>");
             fieldWrapper.data("idx", intId);
 
-            var fName = $("<div class=\"col-md-5\"><select name=\"language_code_source_" + select_count + "\" id=\"language_code_source_" + select_count + "\" class=\"form-select fieldtype\" required=\"required\"><option value>--Select a language--</option>' . $source_lang . '</select></div>");
-            var fType = $("<div class=\"col-md-4\"><select name=\"language_code_target_" + select_count + "\" id=\"language_code_target_" + select_count + "\" class=\"form-select fieldtype\" required=\"required\"><option value>--Select a language--</option>' . $target_lang . '</select></div>");
+            var fName = $("<div class=\"col-md-5\"><select name=\"language_code_source_" + select_count + "\" id=\"language_code_source_" + select_count + "\" class=\"form-select fieldtype\" required=\"required\"><option value>--' . Lib\Localisation::getTranslation('ff_I_can_trans') . '--</option>' . $source_lang . '</select></div>");
+            var fType = $("<div class=\"col-md-4\"><select name=\"language_code_target_" + select_count + "\" id=\"language_code_target_" + select_count + "\" class=\"form-select fieldtype\" required=\"required\"><option value>--' . Lib\Localisation::getTranslation('ff_to_lang') . '--</option>' . $target_lang . '</select></div>");
             var fTypee = $("<div class=\"col-md-2\"><select name=\"qualification_level_" + select_count + "\" id=\"qualification_level_" + select_count + "\" class=\"form-select fieldtype1\"><option value>--Select--</option>' . $qualification_level . '</select></div>");
             var removeButton = $("<div class=\"col-md-1\"><input type=\"button\" class=\"remove btn btn-sm btn-outline-danger\" value=\"-\" /></div>");
 
