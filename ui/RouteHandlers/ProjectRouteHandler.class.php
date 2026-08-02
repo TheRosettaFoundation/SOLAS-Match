807493afa4004e4e4f89a60d43c665e59c17ba80
Alan Barrett <alanabarrett0@gmail.com>
2026-08-02 15:25:00 +0100

    $roles

--- ui/RouteHandlers/ProjectRouteHandler.class.php
+++ ui/RouteHandlers/ProjectRouteHandler.class.php
@@ -1211,6 +1211,7 @@ error_log("task_id: $task_id, memsource_task for {$part['uid']} in event JOB_STA
     {
         $project_id = (int)$args['project_id'];
 
+        $adminDao = new DAO\AdminDao();
         $projectDao = new DAO\ProjectDao();
         $taskDao = new DAO\TaskDao();
 
@@ -1222,6 +1223,7 @@ error_log("task_id: $task_id, memsource_task for {$part['uid']} in event JOB_STA
             if ($data = UserRouteHandler::t_validate($org_id)) {
                 $user_id = $data['user']['id'];
                 if (!$taskDao->isUserRestrictedFromProject($project_id, $user_id) && !in_array($project_id, [36065, 36066, 36067, 36068, 36068, 36072, 36070, 36069, 36071, 36073, 36074, 36075, 36076, 36958, 36963])) {
+                    $roles = $adminDao->get_roles($user_id, $org_id);
                     $post = $request->getParsedBody();
                     if (isset($post['translators_count'])) {
                         $response->getBody()->write(json_encode($taskDao->count_users_who_can_claim($post['translators_count'])));
@@ -1302,6 +1304,7 @@ error_log("task_id: $task_id, memsource_task for {$part['uid']} in event JOB_STA
                     $pm = $creator['email'];
                     if (strpos($pm, '@translatorswithoutborders.org') === false && strpos($pm, '@clearglobal.org') === false) $pm = 'projects@translatorswithoutborders.org';
 
+                    $data['roles'] = $roles;
                     $data['org_name'] = $projectDao->get_project_org_name($project_id);
                     $data['projectTasks'] = $project_tasks;
                     $data['taskMetaData'] = $taskMetaData;
