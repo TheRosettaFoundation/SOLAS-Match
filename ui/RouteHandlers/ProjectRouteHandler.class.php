f3aff36b21bcaf85d2eff21a3aa3cc7dcf7d1045
Alan Barrett <alanabarrett0@gmail.com>
2026-08-18 17:23:01 +0100

    $userDao = new DAO\UserDao();

--- ui/RouteHandlers/ProjectRouteHandler.class.php
+++ ui/RouteHandlers/ProjectRouteHandler.class.php
@@ -1964,6 +1964,7 @@ error_log("task_id: $task_id, memsource_task for {$part['uid']} in event JOB_STA
     {
         $org_id = (int)$args['org_id'];
 
+        $userDao = new DAO\UserDao();
         $projectDao = new DAO\ProjectDao();
 
         if ($data = UserRouteHandler::t_validate($org_id)) {
