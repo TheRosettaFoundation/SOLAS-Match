d1f7d275f2ac11b182f06e1fab443fa6ee1662a3
Alan Barrett <alanabarrett0@gmail.com>
2026-09-16 16:39:01 +0100

    LibAPI

--- ui/RouteHandlers/OrgRouteHandler.class.php
+++ ui/RouteHandlers/OrgRouteHandler.class.php
@@ -7,8 +7,10 @@ use \SolasMatch\UI\Lib as Lib;
 use \SolasMatch\Common as Common;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use \SolasMatch\API\Lib as LibAPI;
 
 require_once __DIR__."/../lib/Validator.php";
+require_once __DIR__ . '/../../api/lib/PDOWrapper.class.php';
 
 class OrgRouteHandler
 {
